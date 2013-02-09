ef49b45c2f9851395d1d3b0ed2c2d0f629ea396a
RJ Swedlow <rj150@hotmail.com>
2013-02-09 13:17:33 -0600

    accumulator load and set bit conditions bug fix

--- vu/vne.h
+++ vu/vne.h
@@ -3,29 +3,35 @@
 void VNE(int vd, int vs, int vt, int element)
 {
     register int i;
+
     VCF[01] = 0x0000;
+    for (i = 0; i < 8; i++)
+    { /* 48 bits left by 16 to use high DW sign bit */
+        VACC[i].q >>= 16;
+        /* VACC[i].q <<= 16; // undo zilmar's ACC hack */
+    }
     for (i = 0; i < 8; i++)
     {
         int sel = element_index[element][i];
-        VACC[i].w[01] = VR[vs].s[i];
-        if (VR[vs].s[i] != VR[vt].s[sel])
+
+        if ((VR[vs].s[i] == VR[vt].s[sel]) && (VCF[01] & (0x0100 << i)))
         {
-            VCF[01] |= 0x0001 << i;
-            continue;
+            VCF[01] &= ~(0x0001 << i);
+            VACC[i].w[00] = VR[vt].s[sel];
         }
-        if (VCF[00] & (0x0100 << i))
+        else
         {
             VCF[01] |= 0x0001 << i;
+            VACC[i].w[00] = VR[vs].s[i];
         }
     }
-    VR[vd].s[00] = VACC[00].w[01];
-    VR[vd].s[01] = VACC[01].w[01];
-    VR[vd].s[02] = VACC[02].w[01];
-    VR[vd].s[03] = VACC[03].w[01];
-    VR[vd].s[04] = VACC[04].w[01];
-    VR[vd].s[05] = VACC[05].w[01];
-    VR[vd].s[06] = VACC[06].w[01];
-    VR[vd].s[07] = VACC[07].w[01];
+    for (i = 0; i < 8; i++)
+        VR[vd].s[i] = (short)VACC[i].q;
+    for (i = 0; i < 8; i++)
+    { /* 48 bits left by 16 to use high DW sign bit */
+        VACC[i].q <<= 16;
+        /* VACC[i].q >>= 16; */
+    }
     VCF[00] = 0x0000;
     return;
 }
