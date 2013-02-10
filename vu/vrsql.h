06eda14b887d3c1eaa2dee5b71c01c423ea9b3cf
RJ Swedlow <rj150@hotmail.com>
2013-02-10 10:57:31 -0600

    simplify VDIV ops for small VR-to-VACC writeback blocks

--- vu/vrsql.h
+++ vu/vrsql.h
@@ -2,7 +2,7 @@
 
 void VRSQL(int vd, int del, int vt, int element)
 {
-    register int i;
+    register int i, j;
 #ifdef FP_CORRECTIONS
     unsigned int old_model;
 #endif
@@ -44,99 +44,24 @@ void VRSQL(int vd, int del, int vt, int element)
             sqr = ~sqr;
         }
     }
-    switch (element)
-    {
-        case 0x0:
-        case 0x1:
-            VACC[00].w[00] = VR[vt].s[00];
-            VACC[01].w[00] = VR[vt].s[01];
-            VACC[02].w[00] = VR[vt].s[02];
-            VACC[03].w[00] = VR[vt].s[03];
-            VACC[04].w[00] = VR[vt].s[04];
-            VACC[05].w[00] = VR[vt].s[05];
-            VACC[06].w[00] = VR[vt].s[06];
-            VACC[07].w[00] = VR[vt].s[07];
-            break;
-        case 0x2:
-            VACC[00].w[00] = VR[vt].s[00];
-            VACC[01].w[00] = VR[vt].s[00];
-            VACC[02].w[00] = VR[vt].s[02];
-            VACC[03].w[00] = VR[vt].s[02];
-            VACC[04].w[00] = VR[vt].s[04];
-            VACC[05].w[00] = VR[vt].s[04];
-            VACC[06].w[00] = VR[vt].s[06];
-            VACC[07].w[00] = VR[vt].s[06];
-            break;
-        case 0x3:
-            VACC[00].w[00] = VR[vt].s[01];
-            VACC[01].w[00] = VR[vt].s[01];
-            VACC[02].w[00] = VR[vt].s[03];
-            VACC[03].w[00] = VR[vt].s[03];
-            VACC[04].w[00] = VR[vt].s[05];
-            VACC[05].w[00] = VR[vt].s[05];
-            VACC[06].w[00] = VR[vt].s[07];
-            VACC[07].w[00] = VR[vt].s[07];
-            break;
-        case 0x4:
-            VACC[00].w[00] = VR[vt].s[00];
-            VACC[01].w[00] = VR[vt].s[00];
-            VACC[02].w[00] = VR[vt].s[00];
-            VACC[03].w[00] = VR[vt].s[00];
-            VACC[04].w[00] = VR[vt].s[04];
-            VACC[05].w[00] = VR[vt].s[04];
-            VACC[06].w[00] = VR[vt].s[04];
-            VACC[07].w[00] = VR[vt].s[04];
-            break;
-        case 0x5:
-            VACC[00].w[00] = VR[vt].s[01];
-            VACC[01].w[00] = VR[vt].s[01];
-            VACC[02].w[00] = VR[vt].s[01];
-            VACC[03].w[00] = VR[vt].s[01];
-            VACC[04].w[00] = VR[vt].s[05];
-            VACC[05].w[00] = VR[vt].s[05];
-            VACC[06].w[00] = VR[vt].s[05];
-            VACC[07].w[00] = VR[vt].s[05];
-            break;
-        case 0x6:
-            VACC[00].w[00] = VR[vt].s[02];
-            VACC[01].w[00] = VR[vt].s[02];
-            VACC[02].w[00] = VR[vt].s[02];
-            VACC[03].w[00] = VR[vt].s[02];
-            VACC[04].w[00] = VR[vt].s[06];
-            VACC[05].w[00] = VR[vt].s[06];
-            VACC[06].w[00] = VR[vt].s[06];
-            VACC[07].w[00] = VR[vt].s[06];
-            break;
-        case 0x7:
-            VACC[00].w[00] = VR[vt].s[03];
-            VACC[01].w[00] = VR[vt].s[03];
-            VACC[02].w[00] = VR[vt].s[03];
-            VACC[03].w[00] = VR[vt].s[03];
-            VACC[04].w[00] = VR[vt].s[07];
-            VACC[05].w[00] = VR[vt].s[07];
-            VACC[06].w[00] = VR[vt].s[07];
-            VACC[07].w[00] = VR[vt].s[07];
-            break;
-        case 0x8:
-        case 0x9:
-        case 0xA:
-        case 0xB:
-        case 0xC:
-        case 0xD:
-        case 0xE:
-        case 0xF:{
-            register short source = VR[vt].s[sel];
-            VACC[00].w[00] = source;
-            VACC[01].w[00] = source;
-            VACC[02].w[00] = source;
-            VACC[03].w[00] = source;
-            VACC[04].w[00] = source;
-            VACC[05].w[00] = source;
-            VACC[06].w[00] = source;
-            VACC[07].w[00] = source;
-            break;
+    if (element == 0x0) /* if (element >> 1 == 00) */
+        for (i = 0; i < 8; i++)
+            VACC[i].w[00] = VR[vt].s[i];
+    else if ((element & 0xE) == 02) /* scalar quarter */
+        for (i = 0; i < 8; i++)
+        {
+            j = (i & 0xE) | (element & 01);
+            VACC[i].w[00] = VR[vt].s[j];
         }
-    }
+    else if ((element & 0xC) == 04) /* scalar half */
+        for (i = 0; i < 8; i++)
+        {
+            j = (i & 0xC) | (element & 03);
+            VACC[i].w[00] = VR[vt].s[j];
+        }
+    else /* if ((element & 0b1000) == 0b1000) /* scalar whole */
+        for (i = 0, j = element & 07; i < 8; i++)
+            VACC[i].w[00] = VR[vt].s[j];
     rsp.square_root_res = sqr;
     VR[vd].s[del & 07] = (short)rsp.square_root_res; /* store low part */
     return;
