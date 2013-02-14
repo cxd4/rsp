e3edd268eb9d06f5d64a4192d3ed297c50615ec5
RJ Swedlow <rj150@hotmail.com>
2013-02-13 18:42:37 -0600

    fixed edge condition where vs + vt was -1, more static flow

--- vu/vcr.h
+++ vu/vcr.h
@@ -2,50 +2,83 @@
 
 void VCR(int vd, int vs, int vt, int element)
 {
-    register int i;
+    int ge, le;
+    register int i, j;
 
-    VCO = 0x0000;
     VCC = 0x0000;
-    VCE = 0x0000;
-    for (i = 0; i < 8; i++)
-    {
-        int sel = element_index[element][i];
-        signed short int paired_source_1 = VR[vt].s[sel];
-        signed short int paired_source_2 = VR[vs].s[i];
-        if ((paired_source_2 ^ paired_source_1) >= 0)
-        {
-            if (paired_source_1 < 0)
+    if (element == 0x0) /* if (element >> 1 == 00) */
+        for (i = 0; i < 8; i++)
+            if ((VR[vs].s[i] ^ VR[vt].s[i]) < 0)
             {
-                VCC |= 0x0001 << i;
-            }
-            if (paired_source_2 < paired_source_1)
-            {
-                VACC[i].s[LO] = paired_source_2;
+                ge = (VR[vt].s[i] < 0);
+                le = (VR[vs].s[i] + VR[vt].s[i] + 1 <= 0);
+                VACC[i].s[LO] = le ? ~VR[vt].s[i] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
             }
             else
             {
-                VACC[i].s[LO] = paired_source_1;
-                VCC |= 0x0100 << i;
+                le = (VR[vt].s[i] < 0);
+                ge = (VR[vs].s[i] - VR[vt].s[i] >= 0);
+                VACC[i].s[LO] = le ? VR[vt].s[i] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
             }
-        }
-        else
+    else if ((element & 0xE) == 02) /* scalar quarter */
+        for (i = 0; i < 8; i++)
         {
-            if (paired_source_1 < 0)
+            j = (i & 0xE) | (element & 01);
+            if ((VR[vs].s[i] ^ VR[vt].s[j]) < 0)
             {
-                VCC |= 0x0100 << i;
-            }
-            if (paired_source_1 + paired_source_2 > 0)
-            {
-                VACC[i].s[LO] = paired_source_2;
+                ge = (VR[vt].s[j] < 0);
+                le = (VR[vs].s[i] + VR[vt].s[j] + 1 <= 0);
+                VACC[i].s[LO] = le ? ~VR[vt].s[i] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
             }
             else
             {
-                VACC[i].s[LO] = ~paired_source_1;
-                VCC |= 0x0001 << i;
+                le = (VR[vt].s[j] < 0);
+                ge = (VR[vs].s[i] - VR[vt].s[j] >= 0);
+                VACC[i].s[LO] = le ? VR[vt].s[j] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
             }
         }
-    }
+    else if ((element & 0xC) == 04) /* scalar half */
+        for (i = 0; i < 8; i++)
+        {
+            j = (i & 0xC) | (element & 03);
+            if ((VR[vs].s[i] ^ VR[vt].s[j]) < 0)
+            {
+                ge = (VR[vt].s[j] < 0);
+                le = (VR[vs].s[i] + VR[vt].s[j] + 1 <= 0);
+                VACC[i].s[LO] = le ? ~VR[vt].s[i] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
+            }
+            else
+            {
+                le = (VR[vt].s[j] < 0);
+                ge = (VR[vs].s[i] - VR[vt].s[j] >= 0);
+                VACC[i].s[LO] = le ? VR[vt].s[j] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
+            }
+        }
+    else /* if ((element & 0b1000) == 0b1000) /* scalar whole */
+        for (i = 0, j = element & 07; i < 8; i++)
+            if ((VR[vs].s[i] ^ VR[vt].s[j]) < 0)
+            {
+                ge = (VR[vt].s[j] < 0);
+                le = (VR[vs].s[i] + VR[vt].s[j] + 1 <= 0);
+                VACC[i].s[LO] = le ? ~VR[vt].s[i] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
+            }
+            else
+            {
+                le = (VR[vt].s[j] < 0);
+                ge = (VR[vs].s[i] - VR[vt].s[j] >= 0);
+                VACC[i].s[LO] = le ? VR[vt].s[j] : VR[vs].s[i];
+                VCC |= (ge << (i + 8)) | (le << (i + 0));
+            }
     for (i = 0; i < 8; i++)
         VR[vd].s[i] = VACC[i].s[LO];
+    VCO = 0x0000;
+    VCE = 0x00;
     return;
 }
