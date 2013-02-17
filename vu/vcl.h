8a98d1f3b4e22a035de92947bb44e811660f7004
RJ Swedlow <rj150@hotmail.com>
2013-02-16 21:49:18 -0600

    expanded hacked algorithm into static field decoders

--- vu/vcl.h
+++ vu/vcl.h
@@ -2,77 +2,156 @@
 
 void VCL(int vd, int vs, int vt, int element)
 {
+    int ge, le;
     register int i;
-
-    for (i = 0; i < 8; i++)
-    {
-        int sel = element_index[element][i];
-        signed short s1 = VR[vs].s[i];
-        signed short s2 = VR[vt].s[sel];
-
-        if (VCO & (0x0001 << i))
+	
+    if (element == 0x0) /* if (element >> 1 == 00) */
+        for (i = 0; i < 8; i++)
         {
-            if (VCO & (0x0100 << i))
+            const int j = i;
+            const unsigned short VS = (unsigned short)VR[vs].s[i];
+            const unsigned short VT = (unsigned short)VR[vt].s[j];
+            const int eq = (((VCO >> (i + 8)) & 1) == 0); /* !(NOTEQUAL) */
+
+            if ((VCO >> i) & 1)
             {
-                VACC[i].s[LO] = (VCC & (0x0001 << i)) ? -s2 : +s1;
+                ge = (VCC >> (i + 8)) & 1;
+                if (eq)
+                {
+                    const int sum = VS + VT;
+                    int ce = (VCE >> i) & 1;
+                    int lz = ((sum & 0x0000FFFF) == 0x00000000);
+                    int uz = ((sum & 0xFFFF0000) == 0x00000000); /* !carryout */
+
+                    le = (ce & (lz | uz)) | (!ce & (lz & uz));
+                }
+                else
+                    le = (VCC >> i) & 1;
+                VACC[i].s[LO] = le ? -VT : VS;
             }
             else
             {
-                if (VCE & (0x0001 << i))
-                {
-                    if (((UINT32)(UINT16)s1 + (UINT32)(UINT16)s2) > 0x10000)
-                    { /* proper fix for Harvest Moon 64, r4 */
-                        VACC[i].s[LO] = s1;
-                        VCC &= ~(0x0001 << i);
-                    }
-                    else
-                    {
-                        VACC[i].s[LO] = -s2;
-                        VCC |= 0x0001 << i;
-                    }
-                }
+                le = (VCC >> i) & 1;
+                if (eq)
+                    ge = (VS - VT >= 0);
                 else
-                {
-                    if ((UINT32)(UINT16)s1 + (UINT32)(UINT16)s2)
-                    { /* вместе с фиксом для Harvest Moon 64, */
-                        VACC[i].s[LO] = s1;
-                        VCC &= ~(0x0001 << i);
-                    } /* чтоб соответствовать pj64 1.4 rsp */
-                    else
-                    {
-                        VACC[i].s[LO] = -s2;
-                        VCC |= 0x0001 << i;
-                    }
-                }
+                    ge = (VCC >> (i + 8)) & 1;
+                VACC[i].s[LO] = ge ? VT : VS;
             }
+            VCC &= ~(0x0101 << i);
+            VCC |= (ge << (i + 8)) | (le << (i + 0));
         }
-        else
+    else if ((element & 0xE) == 02) /* scalar quarter */
+        for (i = 0; i < 8; i++)
         {
-            if (VCO & (0x0100 << i))
+            const int j = (i & 0xE) | (element & 01);
+            const unsigned short VS = (unsigned short)VR[vs].s[i];
+            const unsigned short VT = (unsigned short)VR[vt].s[j];
+            const int eq = (((VCO >> (i + 8)) & 1) == 0); /* !(NOTEQUAL) */
+
+            if ((VCO >> i) & 1)
             {
-                if (VCC & (0x0100 << i))
+                ge = (VCC >> (i + 8)) & 1;
+                if (eq)
                 {
-                    VACC[i].s[LO] = s2;
+                    const int sum = VS + VT;
+                    int ce = (VCE >> i) & 1;
+                    int lz = ((sum & 0x0000FFFF) == 0x00000000);
+                    int uz = ((sum & 0xFFFF0000) == 0x00000000); /* !carryout */
+
+                    le = (ce & (lz | uz)) | (!ce & (lz & uz));
                 }
                 else
-                {
-                    VACC[i].s[LO] = s1;
-                }
+                    le = (VCC >> i) & 1;
+                VACC[i].s[LO] = le ? -VT : VS;
             }
             else
             {
-                const unsigned short flag_offset = 0x0100 << i;
-                VACC[i].s[LO] = ((INT32)(UINT16)s1 < (INT32)(UINT16)s2)
-                               ? s1 : s2;
-                VCC = ((INT32)(UINT16)s1 < (INT32)(UINT16)s2)
-                        ? VCC & ~flag_offset
-                        : VCC | flag_offset;
+                le = (VCC >> i) & 1;
+                if (eq)
+                    ge = (VS - VT >= 0);
+                else
+                    ge = (VCC >> (i + 8)) & 1;
+                VACC[i].s[LO] = ge ? VT : VS;
             }
+            VCC &= ~(0x0101 << i);
+            VCC |= (ge << (i + 8)) | (le << (i + 0));
         }
-    }
+    else if ((element & 0xC) == 04) /* scalar half */
+        for (i = 0; i < 8; i++)
+        {
+            const int j = (i & 0xC) | (element & 03);
+            const unsigned short VS = (unsigned short)VR[vs].s[i];
+            const unsigned short VT = (unsigned short)VR[vt].s[j];
+            const int eq = (((VCO >> (i + 8)) & 1) == 0); /* !(NOTEQUAL) */
+
+            if ((VCO >> i) & 1)
+            {
+                ge = (VCC >> (i + 8)) & 1;
+                if (eq)
+                {
+                    const int sum = VS + VT;
+                    int ce = (VCE >> i) & 1;
+                    int lz = ((sum & 0x0000FFFF) == 0x00000000);
+                    int uz = ((sum & 0xFFFF0000) == 0x00000000); /* !carryout */
+
+                    le = (ce & (lz | uz)) | (!ce & (lz & uz));
+                }
+                else
+                    le = (VCC >> i) & 1;
+                VACC[i].s[LO] = le ? -VT : VS;
+            }
+            else
+            {
+                le = (VCC >> i) & 1;
+                if (eq)
+                    ge = (VS - VT >= 0);
+                else
+                    ge = (VCC >> (i + 8)) & 1;
+                VACC[i].s[LO] = ge ? VT : VS;
+            }
+            VCC &= ~(0x0101 << i);
+            VCC |= (ge << (i + 8)) | (le << (i + 0));
+        }
+    else /* if ((element & 0b1000) == 0b1000) /* scalar whole */
+        for (i = 0; i < 8; i++)
+        {
+            const int j = element & 07;
+            const unsigned short VS = (unsigned short)VR[vs].s[i];
+            const unsigned short VT = (unsigned short)VR[vt].s[j];
+            const int eq = (((VCO >> (i + 8)) & 1) == 0); /* !(NOTEQUAL) */
+
+            if ((VCO >> i) & 1)
+            {
+                ge = (VCC >> (i + 8)) & 1;
+                if (eq)
+                {
+                    const int sum = VS + VT;
+                    int ce = (VCE >> i) & 1;
+                    int lz = ((sum & 0x0000FFFF) == 0x00000000);
+                    int uz = ((sum & 0xFFFF0000) == 0x00000000); /* !carryout */
+
+                    le = (ce & (lz | uz)) | (!ce & (lz & uz));
+                }
+                else
+                    le = (VCC >> i) & 1;
+                VACC[i].s[LO] = le ? -VT : VS;
+            }
+            else
+            {
+                le = (VCC >> i) & 1;
+                if (eq)
+                    ge = (VS - VT >= 0);
+                else
+                    ge = (VCC >> (i + 8)) & 1;
+                VACC[i].s[LO] = ge ? VT : VS;
+            }
+            VCC &= ~(0x0101 << i);
+            VCC |= (ge << (i + 8)) | (le << (i + 0));
+        }
     for (i = 0; i < 8; i++)
         VR[vd].s[i] = VACC[i].s[LO];
     VCO = 0x0000;
-    VCE = 0x0000;
+    VCE = 0x00;
     return;
 }
