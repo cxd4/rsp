4416c2de6fdcae2c607e494d883db77c2ed20e80
RJ Swedlow <rj150@hotmail.com>
2013-02-17 22:56:13 -0600

    confirmed accumulator write-back ammendment for divides

--- vu/vmov.h
+++ vu/vmov.h
@@ -1,106 +1,22 @@
 #include "vu.h"
 
-void VMOV(int vd, int del, int vt, int element)
+void VMOV(int vd, int de, int vt, int e)
 {
-    register int sel;
-    del &= 07;
+    register int i, j;
 
-    sel = element_index[element][del ^ 07];
-    VR[vd].s[del] = VR[vt].s[sel];
-/*
-    switch (element)
-    {
-        case 0x0:
-        case 0x1:
-            VACC[00].w[LO] = VR[vt].s[00];
-            VACC[01].w[LO] = VR[vt].s[01];
-            VACC[02].w[LO] = VR[vt].s[02];
-            VACC[03].w[LO] = VR[vt].s[03];
-            VACC[04].w[LO] = VR[vt].s[04];
-            VACC[05].w[LO] = VR[vt].s[05];
-            VACC[06].w[LO] = VR[vt].s[06];
-            VACC[07].w[LO] = VR[vt].s[07];
-            return;
-        case 0x2:
-            VACC[00].w[LO] = VR[vt].s[00];
-            VACC[01].w[LO] = VR[vt].s[00];
-            VACC[02].w[LO] = VR[vt].s[02];
-            VACC[03].w[LO] = VR[vt].s[02];
-            VACC[04].w[LO] = VR[vt].s[04];
-            VACC[05].w[LO] = VR[vt].s[04];
-            VACC[06].w[LO] = VR[vt].s[06];
-            VACC[07].w[LO] = VR[vt].s[06];
-            return;
-        case 0x3:
-            VACC[00].w[LO] = VR[vt].s[01];
-            VACC[01].w[LO] = VR[vt].s[01];
-            VACC[02].w[LO] = VR[vt].s[03];
-            VACC[03].w[LO] = VR[vt].s[03];
-            VACC[04].w[LO] = VR[vt].s[05];
-            VACC[05].w[LO] = VR[vt].s[05];
-            VACC[06].w[LO] = VR[vt].s[07];
-            VACC[07].w[LO] = VR[vt].s[07];
-            return;
-        case 0x4:
-            VACC[00].w[LO] = VR[vt].s[00];
-            VACC[01].w[LO] = VR[vt].s[00];
-            VACC[02].w[LO] = VR[vt].s[00];
-            VACC[03].w[LO] = VR[vt].s[00];
-            VACC[04].w[LO] = VR[vt].s[04];
-            VACC[05].w[LO] = VR[vt].s[04];
-            VACC[06].w[LO] = VR[vt].s[04];
-            VACC[07].w[LO] = VR[vt].s[04];
-            return;
-        case 0x5:
-            VACC[00].w[LO] = VR[vt].s[01];
-            VACC[01].w[LO] = VR[vt].s[01];
-            VACC[02].w[LO] = VR[vt].s[01];
-            VACC[03].w[LO] = VR[vt].s[01];
-            VACC[04].w[LO] = VR[vt].s[05];
-            VACC[05].w[LO] = VR[vt].s[05];
-            VACC[06].w[LO] = VR[vt].s[05];
-            VACC[07].w[LO] = VR[vt].s[05];
-            return;
-        case 0x6:
-            VACC[00].w[LO] = VR[vt].s[02];
-            VACC[01].w[LO] = VR[vt].s[02];
-            VACC[02].w[LO] = VR[vt].s[02];
-            VACC[03].w[LO] = VR[vt].s[02];
-            VACC[04].w[LO] = VR[vt].s[06];
-            VACC[05].w[LO] = VR[vt].s[06];
-            VACC[06].w[LO] = VR[vt].s[06];
-            VACC[07].w[LO] = VR[vt].s[06];
-            return;
-        case 0x7:
-            VACC[00].w[LO] = VR[vt].s[03];
-            VACC[01].w[LO] = VR[vt].s[03];
-            VACC[02].w[LO] = VR[vt].s[03];
-            VACC[03].w[LO] = VR[vt].s[03];
-            VACC[04].w[LO] = VR[vt].s[07];
-            VACC[05].w[LO] = VR[vt].s[07];
-            VACC[06].w[LO] = VR[vt].s[07];
-            VACC[07].w[LO] = VR[vt].s[07];
-            return;
-        case 0x8:
-        case 0x9:
-        case 0xA:
-        case 0xB:
-        case 0xC:
-        case 0xD:
-        case 0xE:
-        case 0xF:
-        {
-            register short source = VR[vt].s[sel];
-            VACC[00].w[LO] = source;
-            VACC[01].w[LO] = source;
-            VACC[02].w[LO] = source;
-            VACC[03].w[LO] = source;
-            VACC[04].w[LO] = source;
-            VACC[05].w[LO] = source;
-            VACC[06].w[LO] = source;
-            VACC[07].w[LO] = source;
-            return;
-        }
-    }
-*/
+    de &= 07;
+    if (e == 0x0) /* (e == 1) was already filtered. */
+        for (i = 0, j = 0; i < 8; j = ++i)
+            VACC[i].s[LO] = VR[vt].s[j];
+    else if (e < 4)
+        for (i = 0, j = e & 01; i < 8; i++, j |= i & 0xE)
+            VACC[i].s[LO] = VR[vt].s[j];
+    else if (e < 8)
+        for (i = 0, j = e & 03; i < 8; i++, j |= i & 0xC)
+            VACC[i].s[LO] = VR[vt].s[j];
+    else /* if (8 <= e <= 15) */
+        for (i = 0, j = e & 07; i < 8; i++)
+            VACC[i].s[LO] = VR[vt].s[j];
+    VR[vd].s[de] = VACC[de].s[LO];
+    return;
 }
