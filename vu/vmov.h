8e0e360059306e476c9cc188151e9259d5f8614d
RJ Swedlow <rj150@hotmail.com>
2013-02-19 13:59:10 -0600

    help template for inline loop unrolling

--- vu/vmov.h
+++ vu/vmov.h
@@ -4,19 +4,19 @@ void VMOV(int vd, int de, int vt, int e)
 {
     register int i, j;
 
-    de &= 07;
-    if (e == 0x0) /* (e == 1) was already filtered. */
-        for (i = 0, j = 0; i < 8; j = ++i)
-            VACC[i].s[LO] = VR[vt].s[j];
-    else if (e < 4)
-        for (i = 0, j = e & 01; i < 8; i++, j |= i & 0xE)
-            VACC[i].s[LO] = VR[vt].s[j];
+    /* MovIn = (int)VR[vt].s[e & 07]; */
+    if (!e)
+        for (i = 0; i < 8; i++)
+            VACC[i].s[LO] = VR[vt].s[j = i];
+    else if (e < 4) /* e != 1 */
+        for (i = 0, j = e & 01; i < 8; i++)
+            VACC[i].s[LO] = VR[vt].s[j | (i & 0xE)];
     else if (e < 8)
-        for (i = 0, j = e & 03; i < 8; i++, j |= i & 0xC)
-            VACC[i].s[LO] = VR[vt].s[j];
+        for (i = 0, j = e & 03; i < 8; i++)
+            VACC[i].s[LO] = VR[vt].s[j | (i & 0xC)];
     else /* if (8 <= e <= 15) */
         for (i = 0, j = e & 07; i < 8; i++)
             VACC[i].s[LO] = VR[vt].s[j];
-    VR[vd].s[de] = VACC[de].s[LO];
+    VR[vd].s[de & 07] = VACC[de].s[LO];
     return;
 }
