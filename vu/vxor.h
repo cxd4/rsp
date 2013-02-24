5a5ae478893df258715795903c92facbf18e0bd0
RJ Swedlow <rj150@hotmail.com>
2013-02-23 23:16:49 -0600

    quick simplify computational logical vector template

--- vu/vxor.h
+++ vu/vxor.h
@@ -4,28 +4,16 @@ static void VXOR(int vd, int vs, int vt, int element)
 {
     register int i, j;
 
-    if (element == 0x0) /* if (element >> 1 == 00) */
-    {
+    if (!element) /* if (element >> 1 == 00) */
         for (i = 0; i < 8; i++)
-            VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[i];
-    }
-    else if ((element & 0xE) == 02) /* scalar quarter */
-    {
-        for (i = 0; i < 8; i++)
-        {
-            j = (i & 0xE) | (element & 01);
-            VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[j];
-        }
-    }
-    else if ((element & 0xC) == 04) /* scalar half */
-    {
-        for (i = 0; i < 8; i++)
-        {
-            j = (i & 0xC) | (element & 03);
-            VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[j];
-        }
-    }
-    else /* if ((element & 0b1000) == 0b1000) /* scalar whole */
+            VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[j = i];
+    else if (element < 4)
+        for (i = 0, j = element & 01; i < 8; i++)
+            VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[j | (i & 0xE)];
+    else if (element < 8)
+        for (i = 0, j = element & 03; i < 8; i++)
+            VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[j | (i & 0xC)];
+    else /* if (element & 0b1000) */
         for (i = 0, j = element & 07; i < 8; i++)
             VACC[i].s[LO] = VR[vs].s[i] ^ VR[vt].s[j];
     for (i = 0; i < 8; i++)
