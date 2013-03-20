a9c895cb39157500795ab26a6af2849681f11ff5
RJ Swedlow <rj150@hotmail.com>
2013-03-20 18:52:37 -0500

    fixed bad (but official patent doc.) length condition formula

--- su/lwc2/lqv.h
+++ su/lwc2/lqv.h
@@ -1,61 +1,74 @@
 /******************************************************************************\
-* Project:  SP VU Emulation Table:  Load Quadword to Vector Unit (LQV)         *
-* Creator:  R. J. Swedlow                                                      *
-* Release:  2012.12.28                                                         *
+* Project:  SP VU Emulation Table:  Load Quadword to Vector Unit               *
+* Authors:  Iconoclast                                                         *
+* Release:  2013.03.20                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
 void LQV(int vt, int element, signed offset, int base)
 {
-    register unsigned int addr = SR[base] + (offset * 16);
-    const unsigned b = addr & 0x00000007;
+    register unsigned int addr;
+    int b;
 
+    addr  = SR[base] + (offset << 4);
     addr &= 0x00000FFF;
+    b = addr & 0x0000000F;
+/* There is a mistake in United States patent no. 5,812,147 by SGI.
+ * In multiple other statements, LQV is defined to go all the way to addr|15.
+ * This means that if addr&15 is 0, length == 16; addr&15 is 15, length == 1.
+ *
+ * However, they give an if-else chain for b={0:7} only, which allows a bug
+ * exploitable by Resident Evil 2 and, possibly, some Boss Game publications.
+ */
     if (element != 0x0)
-    { /* to-do:  check RE2 prologue cinnema */
-        message("LQV\nWeird element.", 3);
+    {
+        message("LQV\nIllegal element.", 3);
         return;
     }
-    switch (b)
+    switch (addr & 0x0000000F)
     {
-        case 00:
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x0 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x2 ^ 02));
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x4 ^ 02));
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x6 ^ 02));
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x8 ^ 02));
-            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0xA ^ 02));
-            VR[vt][06] = *(short *)(RSP.DMEM + addr + (0xC ^ 02));
-            VR[vt][07] = *(short *)(RSP.DMEM + addr + (0xE ^ 02));
+        case 0x0:
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
+            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][06] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][07] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
-        case 02:
-            addr -= 0x002;
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x2 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x4 ^ 02));
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x6 ^ 02));
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x8 ^ 02));
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0xA ^ 02));
-            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0xC ^ 02));
-            VR[vt][06] = *(short *)(RSP.DMEM + addr + (0xE ^ 02));
+        case 0x2:
+            VR[vt][00] = *(short *)(RSP.DMEM + addr - 0x002);
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + 0x004);
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + 0x002);
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + 0x008);
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + 0x006);
+            VR[vt][05] = *(short *)(RSP.DMEM + addr + 0x00C);
+            VR[vt][06] = *(short *)(RSP.DMEM + addr + 0x00A);
             return;
-        case 04:
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x0 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x2 ^ 02));
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x4 ^ 02));
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x6 ^ 02));
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x8 ^ 02));
-            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0xA ^ 02));
+        case 0x4:
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
+            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
             return;
-        case 06: /* Very rarely is b != 0, but Mario 64 title intro b == 6. */
-            addr -= 0x002;
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x2 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x4 ^ 02));
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x6 ^ 02));
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x8 ^ 02));
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0xA ^ 02));
+        case 0x6:
+            VR[vt][00] = *(short *)(RSP.DMEM + addr - 0x002);
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + 0x004);
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + 0x002);
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + 0x008);
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + 0x006);
+            return;
+        case 0x8: /* Resident Evil 2 cinematics and Boss Game Studios */
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
             return;
         default:
-            message("LQV\nUnaligned addr.", 3);
+            message("LQV\nWeird addr.", 3);
             return;
     }
 }
