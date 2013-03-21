6d31df178e31686f665095be23a20222cb5150b8
RJ Swedlow <rj150@hotmail.com>
2013-03-21 10:02:14 -0500

    static condition codes for unsigned clamp

--- vu/vmulu.h
+++ vu/vmulu.h
@@ -6,12 +6,11 @@ static void VMULU(int vd, int vs, int vt, int e)
 
     for (i = 0; i < 8; i++)
         VACC[i].DW = (VR[vs][i]*VR[vt][ei[e][i]] << 1) + 0x8000;
-    for (i = 0; i < 8; i++) /* Unsigned-clamp bits 31..16 of ACC to dest. VR. */
-        if (VACC[i].DW < 0)
-            VR[vd][i] = 0x0000; /* unsigned underflow */
-        else if (VACC[i].W[0] == 0x80008000) /* only reachable overflow value */
-            VR[vd][i] = 0xFFFF;
-        else
-            VR[vd][i] = VACC[i].s[MD];
+    for (i = 0; i < 8; i++) /* Unsign-clamp bits 31..16 of ACC to dest. VR. */
+    {
+        VR[vd][i] = VACC[i].s[MD];
+        VR[vd][i] |= VR[vd][i] >> 15; /* VD |= -1 * (ACC > 0x00007FFFFFFF); */
+        VR[vd][i] &= ~VACC[i].s[HI]; /* VD = (ACC < 0) ? 0 : ACC[31..16]; */
+    }
     return;
 }
