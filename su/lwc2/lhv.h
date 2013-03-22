aa6243fdd254c8475eb8f284927a426a0cc8aa6d
RJ Swedlow <rj150@hotmail.com>
2013-03-22 14:44:59 -0500

    small speed-up to Resident Evil 2 MPEGs

--- su/lwc2/lhv.h
+++ su/lwc2/lhv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Load Alternate Bytes to Vector Unit        *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.03.21                                                         *
+* Release:  2013.03.22                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -13,34 +13,30 @@ void LHV(int vt, int element, signed int offset, int base)
     addr &= 0x00000FFF;
     if (element != 0x0) /* We need an explicit `goto` for stupid compilers. */
         goto bitch; /* Blame M$ for their ineptitude with branch weighs. */
-    switch (addr & 0x0000000F)
-    {
-        case 0x0:
-            VR[vt][07] = RSP.DMEM[addr + (0xF ^ 02)] << 7;
-            VR[vt][06] = RSP.DMEM[addr + (0xD ^ 02)] << 7;
-            VR[vt][05] = RSP.DMEM[addr + (0xB ^ 02)] << 7;
-            VR[vt][04] = RSP.DMEM[addr + (0x9 ^ 02)] << 7;
-            VR[vt][03] = RSP.DMEM[addr + (0x7 ^ 02)] << 7;
-            VR[vt][02] = RSP.DMEM[addr + (0x5 ^ 02)] << 7;
-            VR[vt][01] = RSP.DMEM[addr + (0x3 ^ 02)] << 7;
-            VR[vt][00] = RSP.DMEM[addr + (0x1 ^ 02)] << 7;
-            return;
-        case 0x1:
-            --addr;
-            VR[vt][07] = RSP.DMEM[addr + (0xE ^ 02)] << 7;
-            VR[vt][06] = RSP.DMEM[addr + (0xC ^ 02)] << 7;
-            VR[vt][05] = RSP.DMEM[addr + (0xA ^ 02)] << 7;
-            VR[vt][04] = RSP.DMEM[addr + (0x8 ^ 02)] << 7;
-            VR[vt][03] = RSP.DMEM[addr + (0x6 ^ 02)] << 7;
-            VR[vt][02] = RSP.DMEM[addr + (0x4 ^ 02)] << 7;
-            VR[vt][01] = RSP.DMEM[addr + (0x2 ^ 02)] << 7;
-            VR[vt][00] = RSP.DMEM[addr + (0x0 ^ 02)] << 7;
-            return;
-        default:
-            message("LHV\nIllegal addr.", 3);
-            return;
-    }
+    if (addr & 0x0000000E)
+        goto ass;
+/* The official algorithm for ?HV as defined in US patent no. 5,812,147 first
+ * checks the least significant bit of `addr` to specify whether to load
+ * every even byte or every odd byte.
+ *
+ * More computationally efficient, however, is to encompass that dynamic
+ * check by statically emulating the halfword index intervals `+= 0x002`.
+ * Don't forget to swap the endianness of the halfword index barrier `^= 02`.
+ */
+    addr ^= 0x001; /* Because MIPS is bi-endian, and Intel isn't. */
+    VR[vt][07] = RSP.DMEM[addr + (0x00E ^ 02)] << 7;
+    VR[vt][06] = RSP.DMEM[addr + (0x00C ^ 02)] << 7;
+    VR[vt][05] = RSP.DMEM[addr + (0x00A ^ 02)] << 7;
+    VR[vt][04] = RSP.DMEM[addr + (0x008 ^ 02)] << 7;
+    VR[vt][03] = RSP.DMEM[addr + (0x006 ^ 02)] << 7;
+    VR[vt][02] = RSP.DMEM[addr + (0x004 ^ 02)] << 7;
+    VR[vt][01] = RSP.DMEM[addr + (0x002 ^ 02)] << 7;
+    VR[vt][00] = RSP.DMEM[addr + (0x000 ^ 02)] << 7;
+    return;
 bitch:
     message("LHV\nIllegal element.", 3);
+    return;
+ass:
+    message("LHV\nIllegal addr.", 3);
     return;
 }
--- su/swc2/shv.h
+++ su/swc2/shv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Store Alternate Bytes from Vector Unit     *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.03.21                                                         *
+* Release:  2013.03.22                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -13,34 +13,30 @@ void SHV(int vt, int element, signed int offset, int base)
     addr &= 0x00000FFF;
     if (element != 0x0) /* We need an explicit `goto` for stupid compilers. */
         goto bitch; /* Blame M$ for their ineptitude with branch weighs. */
-    switch (addr & 0x0000000F)
-    {
-        case 0x0:
-            RSP.DMEM[addr + (0x00F ^ 02)] = (unsigned char)(VR[vt][07] >> 7);
-            RSP.DMEM[addr + (0x00D ^ 02)] = (unsigned char)(VR[vt][06] >> 7);
-            RSP.DMEM[addr + (0x00B ^ 02)] = (unsigned char)(VR[vt][05] >> 7);
-            RSP.DMEM[addr + (0x009 ^ 02)] = (unsigned char)(VR[vt][04] >> 7);
-            RSP.DMEM[addr + (0x007 ^ 02)] = (unsigned char)(VR[vt][03] >> 7);
-            RSP.DMEM[addr + (0x005 ^ 02)] = (unsigned char)(VR[vt][02] >> 7);
-            RSP.DMEM[addr + (0x003 ^ 02)] = (unsigned char)(VR[vt][01] >> 7);
-            RSP.DMEM[addr + (0x001 ^ 02)] = (unsigned char)(VR[vt][00] >> 7);
-            return;
-        case 0x1:
-            --addr;
-            RSP.DMEM[addr + (0x00E ^ 02)] = (unsigned char)(VR[vt][07] >> 7);
-            RSP.DMEM[addr + (0x00C ^ 02)] = (unsigned char)(VR[vt][06] >> 7);
-            RSP.DMEM[addr + (0x00A ^ 02)] = (unsigned char)(VR[vt][05] >> 7);
-            RSP.DMEM[addr + (0x008 ^ 02)] = (unsigned char)(VR[vt][04] >> 7);
-            RSP.DMEM[addr + (0x006 ^ 02)] = (unsigned char)(VR[vt][03] >> 7);
-            RSP.DMEM[addr + (0x004 ^ 02)] = (unsigned char)(VR[vt][02] >> 7);
-            RSP.DMEM[addr + (0x002 ^ 02)] = (unsigned char)(VR[vt][01] >> 7);
-            RSP.DMEM[addr + (0x000 ^ 02)] = (unsigned char)(VR[vt][00] >> 7);
-            return;
-        default:
-            message("SHV\nIllegal addr.", 3);
-            return;
-    }
+    if (addr & 0x0000000E)
+        goto ass;
+/* The official algorithm for ?HV as defined in US patent no. 5,812,147 first
+ * checks the least significant bit of `addr` to specify whether to load
+ * every even byte or every odd byte.
+ *
+ * More computationally efficient, however, is to encompass that dynamic
+ * check by statically emulating the halfword index intervals `+= 0x002`.
+ * Don't forget to swap the endianness of the halfword index barrier `^= 02`.
+ */
+    addr ^= 0x001; /* Because MIPS is bi-endian, and Intel isn't. */
+    RSP.DMEM[addr + (0x00E ^ 02)] = (unsigned char)(VR[vt][07] >> 7);
+    RSP.DMEM[addr + (0x00C ^ 02)] = (unsigned char)(VR[vt][06] >> 7);
+    RSP.DMEM[addr + (0x00A ^ 02)] = (unsigned char)(VR[vt][05] >> 7);
+    RSP.DMEM[addr + (0x008 ^ 02)] = (unsigned char)(VR[vt][04] >> 7);
+    RSP.DMEM[addr + (0x006 ^ 02)] = (unsigned char)(VR[vt][03] >> 7);
+    RSP.DMEM[addr + (0x004 ^ 02)] = (unsigned char)(VR[vt][02] >> 7);
+    RSP.DMEM[addr + (0x002 ^ 02)] = (unsigned char)(VR[vt][01] >> 7);
+    RSP.DMEM[addr + (0x000 ^ 02)] = (unsigned char)(VR[vt][00] >> 7);
+    return;
 bitch:
     message("SHV\nIllegal element.", 3);
+    return;
+ass:
+    message("SHV\nIllegal addr.", 3);
     return;
 }
