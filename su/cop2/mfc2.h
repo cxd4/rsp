6f9ff3a9667c911b1e283151de52effc43226804
RJ Swedlow <rj150@hotmail.com>
2013-03-23 18:01:50 -0500

    forgot to apply 16-bit dynamic pointer speed-up to M?C2

--- su/cop2/mfc2.h
+++ su/cop2/mfc2.h
@@ -2,36 +2,20 @@
 
 void MFC2(int rt, int vd, int element)
 {
+/*
     if (rt == 0)
-    {
-        message("MFC2\t$zero, vd", 1);
-        SR[rt] = 0x00000000;
-        return;
-    }
-    if ((element & 0x1) != 0x0)
-    {
-        message("MFC2\nUnaligned element.", 0); /* A few games will try this. */
-        element = (unsigned int)(element) >> 1;
-        SR[rt]  = (signed char)(VR[vd][element]) << 8;
-        element += 0x1;
-        element &= 0xF;
-        SR[rt] |= (unsigned char)(VR[vd][element] >> 8);
-        return;
-    }
-    element = (unsigned int)(element) >> 1;
-    SR[rt] = (signed short)VR[vd][element];
+        return; // zero permanence already handled in main CPU loop
+*/
+    if (element == 0xF)
+        goto cross;
+    SR[rt] = VR_S(vd, element);
+    SR[rt] = (signed short)(SR[rt]);
+    return;
+cross:
+    message("MFC2\nCrossed segment allocation barrier.", 3); // experimental?
+/*
+    SR[rt] = (VR_B(vd, 0xF) << 8) | VR_B(vd, 0x0);
+    SR[rt] = (signed short)(SR[rt]);
+ */
     return;
 }
-
-/* Note about vector registers.
- * RSP vectors are just like standard mathematical vectors, big endian.
- * It is correct to have them as big-endian arrays, with bits in little.
- *
- * I have not yet figured out how to address 128-bit vectors by byte quickly.
- * The legacy of RSP emulation has always used unions to have such
- * constructors predefined, but this inverted the vector indexing endian.
- * There is no necessity for Objective-C or unions to define vectors.
- *
- * lbv, ldv, sbv, ssv, sdv, spv, and `cop2::mfc2` all require byte-indexing
- * for the demands of the standard F3DEX graphics microcodes.
- */
--- su/cop2/mtc2.h
+++ su/cop2/mtc2.h
@@ -2,12 +2,6 @@
 
 void MTC2(int rt, int vd, int element)
 {
-    if ((element & 0x1) != 0x0)
-    {
-        message("MTC2\nUnaligned element.", 3);
-        return;
-    }
-    element = (unsigned int)(element) >> 1;
-    VR[vd][element] = (short)SR[rt];
-    return;
+    VR_S(vd, element) = (short)SR[rt];
+    return; /* If element = 0xF, it does not matter; loads do not wrap over. */
 }
