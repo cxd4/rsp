dbd23f9c985192bb3c6343ca4643c3afc12f4c37
RJ Swedlow <rj150@hotmail.com>
2013-03-23 18:04:53 -0500

    Pokemon Puzzle League HVQM used every possible addr alignment

--- su/lwc2/luv.h
+++ su/lwc2/luv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Load Packed Unsigned to Vector Unit        *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.03.21                                                         *
+* Release:  2013.03.23                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -25,8 +25,89 @@ void LUV(int vt, int element, signed int offset, int base)
             VR[vt][01] = RSP.DMEM[addr + (0x001 ^ 03)] << 7;
             VR[vt][00] = RSP.DMEM[addr + (0x000 ^ 03)] << 7;
             return;
-        default: /* Completely legal, just never seen it be done. */
-            message("LUV\nWeird addr.", 3);
+        case 01: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr + (0x000 ^ 01)] << 7;
+            VR[vt][01] = RSP.DMEM[addr + (0x001 ^ 01)] << 7;
+            VR[vt][02] = RSP.DMEM[addr - 0x001] << 7;
+            VR[vt][03] = RSP.DMEM[addr + 0x006] << 7;
+            VR[vt][04] = RSP.DMEM[addr + 0x005] << 7;
+            VR[vt][05] = RSP.DMEM[addr + 0x004] << 7;
+            VR[vt][06] = RSP.DMEM[addr + 0x003] << 7;
+            addr += 0x007 + 03; /* byte endian swap adjust */
+            addr &= 0x00000FFF;
+            VR[vt][07] = RSP.DMEM[addr] << 7;
+            return;
+        case 02: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr - 0x001] << 7;
+            VR[vt][01] = RSP.DMEM[addr - 0x002] << 7;
+            VR[vt][02] = RSP.DMEM[addr + (0x002 ^ 07)] << 7;
+            VR[vt][03] = RSP.DMEM[addr + (0x003 ^ 07)] << 7;
+            VR[vt][04] = RSP.DMEM[addr + (0x004 ^ 07)] << 7;
+            VR[vt][05] = RSP.DMEM[addr + (0x005 ^ 07)] << 7;
+            addr += 0x006 + 02; /* halfword endian swap adjust */
+            addr &= 0x00000FFF;
+            VR[vt][06] = RSP.DMEM[addr + 0x001] << 7;
+            VR[vt][07] = RSP.DMEM[addr | 0x000] << 7;
+            return;
+        case 03: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr - (0x000 ^ 03)] << 7;
+            VR[vt][01] = RSP.DMEM[addr + 0x004] << 7;
+            VR[vt][02] = RSP.DMEM[addr + 0x003] << 7;
+            VR[vt][03] = RSP.DMEM[addr + 0x002] << 7;
+            VR[vt][04] = RSP.DMEM[addr + 0x001] << 7;
+            addr += 0x005 + 01; /* Make sure at least one target is base + 0. */
+            addr &= 0x00000FFF;
+            VR[vt][05] = RSP.DMEM[addr + (0x001 ^ 03)] << 7;
+            VR[vt][06] = RSP.DMEM[addr + (0x002 ^ 03)] << 7;
+            VR[vt][07] = RSP.DMEM[addr + (0x003 ^ 03)] << 7;
+            return;
+        case 04: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr + (0x000 ^ 03)] << 7;
+            VR[vt][01] = RSP.DMEM[addr + (0x001 ^ 03)] << 7;
+            VR[vt][02] = RSP.DMEM[addr + (0x002 ^ 03)] << 7;
+            VR[vt][03] = RSP.DMEM[addr + (0x003 ^ 03)] << 7;
+            addr += 0x004;
+            addr &= 0x00000FFF;
+            VR[vt][04] = RSP.DMEM[addr + (0x000 ^ 03)] << 7;
+            VR[vt][05] = RSP.DMEM[addr + (0x001 ^ 03)] << 7;
+            VR[vt][06] = RSP.DMEM[addr + (0x002 ^ 03)] << 7;
+            VR[vt][07] = RSP.DMEM[addr + (0x003 ^ 03)] << 7;
+            return;
+        case 05: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr + 0x001] << 7;
+            VR[vt][01] = RSP.DMEM[addr | 0x000] << 7;
+            VR[vt][02] = RSP.DMEM[addr - 0x001] << 7;
+            addr += 0x003;
+            addr &= 0x00000FFF;
+            VR[vt][03] = RSP.DMEM[addr + (0x000 ^ 03)] << 7;
+            VR[vt][04] = RSP.DMEM[addr + (0x001 ^ 03)] << 7;
+            VR[vt][05] = RSP.DMEM[addr + (0x002 ^ 03)] << 7;
+            VR[vt][06] = RSP.DMEM[addr + (0x003 ^ 03)] << 7;
+            VR[vt][07] = RSP.DMEM[addr + (0x004 ^ 03)] << 7;
+            return;
+        case 06: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr - 0x001] << 7;
+            VR[vt][01] = RSP.DMEM[addr - 0x002] << 7;
+            addr += 0x002;
+            addr &= 0x00000FFF;
+            VR[vt][02] = RSP.DMEM[addr + (0x000 ^ 03)] << 7;
+            VR[vt][03] = RSP.DMEM[addr + (0x001 ^ 03)] << 7;
+            VR[vt][04] = RSP.DMEM[addr + (0x002 ^ 03)] << 7;
+            VR[vt][05] = RSP.DMEM[addr + (0x003 ^ 03)] << 7;
+            VR[vt][06] = RSP.DMEM[addr + (0x004 ^ 03)] << 7;
+            VR[vt][07] = RSP.DMEM[addr + (0x005 ^ 03)] << 7;
+            return;
+        case 07: /* PKMN Puzzle League HVQM decoder */
+            VR[vt][00] = RSP.DMEM[addr - (0x000 ^ 03)] << 7;
+            addr += 0x001;
+            addr &= 0x00000FFF;
+            VR[vt][01] = RSP.DMEM[addr + 0x003] << 7;
+            VR[vt][02] = RSP.DMEM[addr + 0x002] << 7;
+            VR[vt][03] = RSP.DMEM[addr + 0x001] << 7;
+            VR[vt][04] = RSP.DMEM[addr + 0x000] << 7;
+            VR[vt][05] = RSP.DMEM[addr + 0x007] << 7;
+            VR[vt][06] = RSP.DMEM[addr + 0x006] << 7;
+            VR[vt][07] = RSP.DMEM[addr + 0x005] << 7;
             return;
     }
 bitch:
