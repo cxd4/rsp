e9dd0e177a73f8b99b72f3f1f97434601d0649c4
RJ Swedlow <rj150@hotmail.com>
2013-03-23 23:05:48 -0500

    temp working fix to Conker's Bad Fur Day

--- su/cop2/mfc2.h
+++ su/cop2/mfc2.h
@@ -7,15 +7,13 @@ void MFC2(int rt, int vd, int element)
         return; // zero permanence already handled in main CPU loop
 */
     if (element == 0xF)
-        goto cross;
+        goto leap;
     SR[rt] = VR_S(vd, element);
     SR[rt] = (signed short)(SR[rt]);
     return;
-cross:
-    message("MFC2\nCrossed segment allocation barrier.", 3); // experimental?
-/*
+leap:
+    message("MFC2\nCrossed segment allocation barrier.", 0);
     SR[rt] = (VR_B(vd, 0xF) << 8) | VR_B(vd, 0x0);
     SR[rt] = (signed short)(SR[rt]);
- */
     return;
 }
