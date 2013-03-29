412bc6f93db0bac03a611d78a136c869cfebf218
RJ Swedlow <rj150@hotmail.com>
2013-03-28 22:46:19 -0500

    more reliable 64-bit accumulator superset to fetch sign

--- vu/vmulu.h
+++ vu/vmulu.h
@@ -6,11 +6,11 @@ static void VMULU(int vd, int vs, int vt, int e)
 
     for (i = 0; i < 8; i++)
         VACC[i].DW = (VR[vs][i]*VR[vt][ei[e][i]] << 1) + 0x8000;
-    for (i = 0; i < 8; i++) /* Unsign-clamp bits 31..16 of ACC to dest. VR. */
+    for (i = 0; i < 8; i++) /* Zero-clamp bits 31..16 of ACC to dest. VR. */
     {
-        VR[vd][i] = VACC[i].s[MD];
-        VR[vd][i] |= VR[vd][i] >> 15; /* VD |= -1 * (ACC > 0x00007FFFFFFF); */
-        VR[vd][i] &= ~VACC[i].s[HI]; /* VD = (ACC < 0) ? 0 : ACC[31..16]; */
+        VR[vd][i] = VACC[i].s[MD]; /* VD  = ACC[31..16] */
+        VR[vd][i] |= VR[vd][i] >> 15; /* VD |= -(result == 0x80008000) */
+        VR[vd][i] &= ~VACC[i].HW[03]; /* VD  = (ACC < 0) ? 0 : ACC[31..16]; */
     }
     return;
 }
