18af0cf45c786f2a718ecdf652703f4c07b6772f
RJ Swedlow <rj150@hotmail.com>
2013-03-28 22:54:17 -0500

    faster, direct unsigned clamping (F3DZEX and F3DEX)

--- vu/vmacu.h
+++ vu/vmacu.h
@@ -6,12 +6,17 @@ static void VMACU(int vd, int vs, int vt, int e)
 
     for (i = 0; i < 8; i++)
         VACC[i].DW += VR[vs][i]*VR[vt][ei[e][i]] << 1;
-    for (i = 0; i < 8; i++) /* Unsigned-clamp bits 31..16 of ACC to dest. VR. */
-        if (VACC[i].DW & 0x800000000000) /* acc < 0 */
-            VR[vd][i] = 0x0000; /* unsigned underflow */
-        else if (VACC[i].DW & ~0x00007FFFFFFF) /* short overflow */
-            VR[vd][i] = 0xFFFF;
-        else
-            VR[vd][i] = VACC[i].s[MD];
+    for (i = 0; i < 8; i++) /* Zero-clamp bits 31..16 of ACC to dest. VR. */
+    {
+        register signed short result;
+        register short int tmp;
+
+        result  = VACC[i].s[MD]; /* raw slice before clamping */
+        tmp = (signed short)(VACC[i].DW >> 31) != 0x0000;
+        result |= -tmp; /* slice overflow */
+        tmp = VACC[i].s[HI] >> 15; /* Zero- or one-extend. */
+        result &= ~tmp; /* slice underflow */
+        VR[vd][i] = result;
+    }
     return;
 }
