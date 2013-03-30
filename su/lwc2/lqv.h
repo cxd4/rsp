11151ba3300035f5f56424a647154a1258880a5d
RJ Swedlow <rj150@hotmail.com>
2013-03-30 09:07:57 -0500

    less error-prone coding, small clarity upgrade

--- su/lwc2/lqv.h
+++ su/lwc2/lqv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Load Quadword to Vector Unit               *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.03.23                                                         *
+* Release:  2013.03.30                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -11,8 +11,8 @@ void LQV(int vt, int element, signed int offset, int base)
     int b;
 
     addr  = SR[base] + (offset << 4);
-    addr &= 0x00000FFF;
     b = addr & 0x0000000F;
+    addr &= 0x00000FF0;
 /* There is a mistake in United States patent no. 5,812,147 by SGI.
  * In multiple other statements, LQV is defined to go all the way to addr|15.
  * This means that if addr&15 is 0, length == 16; addr&15 is 15, length == 1.
@@ -22,7 +22,7 @@ void LQV(int vt, int element, signed int offset, int base)
  * Conker's Bad Fur Day audio also could have exploited this but covers it.
  */
     if (element != 0x0) /* We need an explicit `goto` for stupid compilers. */
-        goto bitch; /* Blame M$ for their ineptitude with branch weighs. */
+        goto ILLEGAL; /* Illegal vector inst but not an invalid N64 RCP inst. */
     switch (b)
     {
         case 0x0:
@@ -36,52 +36,56 @@ void LQV(int vt, int element, signed int offset, int base)
             VR[vt][07] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0x2:
-            VR[vt][00] = *(short *)(RSP.DMEM + addr - (0x000 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + 0x004);
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + 0x002);
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + 0x008);
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + 0x006);
-            VR[vt][05] = *(short *)(RSP.DMEM + addr + 0x00C);
-            VR[vt][06] = *(short *)(RSP.DMEM + addr + 0x00A);
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][06] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0x4:
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
-            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][05] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0x6:
-            VR[vt][00] = *(short *)(RSP.DMEM + addr - (0x000 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + 0x004);
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + 0x002);
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + 0x008);
-            VR[vt][04] = *(short *)(RSP.DMEM + addr + 0x006);
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][04] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0x8: /* Resident Evil 2 cinematics and Boss Game Studios */
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x004 ^ 02));
-            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x006 ^ 02));
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x008 ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][03] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0xA: /* "Conker's Bad Fur Day" audio microcode by Rareware */
-            VR[vt][00] = *(short *)(RSP.DMEM + addr - (0x000 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + 0x004);
-            VR[vt][02] = *(short *)(RSP.DMEM + addr + 0x002);
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x00A ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][02] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0xC: /* "Conker's Bad Fur Day" audio microcode by Rareware */
-            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
-            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x00C ^ 02));
+            VR[vt][01] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         case 0xE: /* "Conker's Bad Fur Day" audio microcode by Rareware */
-            VR[vt][00] = *(short *)(RSP.DMEM + addr - (0x000 ^ 02));
+            VR[vt][00] = *(short *)(RSP.DMEM + addr + (0x00E ^ 02));
             return;
         default:
             message("LQV\nOdd addr.", 3);
             return;
     }
-bitch:
+/* Obviously, an alternative "fall-through" version of this switch could be
+ * written by removing `addr &= 0x00000FF0;` and adjusting all offsets
+ * accordingly, but this is slow and harder to optimize for several reasons.
+ */
+ILLEGAL:
     message("LQV\nIllegal element.", 3);
     return;
 }
--- su/swc2/sqv.h
+++ su/swc2/sqv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Store Quadword from Vector Unit            *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.03.21                                                         *
+* Release:  2013.03.30                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -11,10 +11,10 @@ void SQV(int vt, int element, signed int offset, int base)
     int b;
 
     addr  = SR[base] + (offset << 4);
-    addr &= 0x00000FFF; /* World Driver Championship:  on load demo intro */
     b = addr & 0x0000000F; /* experimental; patent says should & 07 */
+    addr &= 0x00000FF0; /* World Driver Championship:  on load demo intro */
     if (element != 0x0) /* We need an explicit `goto` for stupid compilers. */
-        goto bitch; /* Blame M$ for their ineptitude with branch weighs. */
+        goto ILLEGAL; /* Illegal vector inst but not an invalid N64 RCP inst. */
     switch (b)
     {
         case 00:
@@ -28,34 +28,34 @@ void SQV(int vt, int element, signed int offset, int base)
             *(short *)(RSP.DMEM + addr + (0x00E ^ 02)) = VR[vt][07];
             return;
         case 02:
-            *(short *)(RSP.DMEM + addr - 0x002) = VR[vt][00];
-            *(short *)(RSP.DMEM + addr + 0x004) = VR[vt][01];
-            *(short *)(RSP.DMEM + addr + 0x002) = VR[vt][02];
-            *(short *)(RSP.DMEM + addr + 0x008) = VR[vt][03];
-            *(short *)(RSP.DMEM + addr + 0x006) = VR[vt][04];
-            *(short *)(RSP.DMEM + addr + 0x00C) = VR[vt][05];
-            *(short *)(RSP.DMEM + addr + 0x00A) = VR[vt][06];
+            *(short *)(RSP.DMEM + addr + (0x002 ^ 02)) = VR[vt][00];
+            *(short *)(RSP.DMEM + addr + (0x004 ^ 02)) = VR[vt][01];
+            *(short *)(RSP.DMEM + addr + (0x006 ^ 02)) = VR[vt][02];
+            *(short *)(RSP.DMEM + addr + (0x008 ^ 02)) = VR[vt][03];
+            *(short *)(RSP.DMEM + addr + (0x00A ^ 02)) = VR[vt][04];
+            *(short *)(RSP.DMEM + addr + (0x00C ^ 02)) = VR[vt][05];
+            *(short *)(RSP.DMEM + addr + (0x00E ^ 02)) = VR[vt][06];
             return;
         case 04:
-            *(short *)(RSP.DMEM + addr + 0x002) = VR[vt][00];
-            *(short *)(RSP.DMEM + addr + 0x000) = VR[vt][01];
-            *(short *)(RSP.DMEM + addr + 0x006) = VR[vt][02];
-            *(short *)(RSP.DMEM + addr + 0x004) = VR[vt][03];
-            *(short *)(RSP.DMEM + addr + 0x00A) = VR[vt][04];
-            *(short *)(RSP.DMEM + addr + 0x008) = VR[vt][05];
+            *(short *)(RSP.DMEM + addr + (0x004 ^ 02)) = VR[vt][00];
+            *(short *)(RSP.DMEM + addr + (0x006 ^ 02)) = VR[vt][01];
+            *(short *)(RSP.DMEM + addr + (0x008 ^ 02)) = VR[vt][02];
+            *(short *)(RSP.DMEM + addr + (0x00A ^ 02)) = VR[vt][03];
+            *(short *)(RSP.DMEM + addr + (0x00C ^ 02)) = VR[vt][04];
+            *(short *)(RSP.DMEM + addr + (0x00E ^ 02)) = VR[vt][05];
             return;
         case 06:
-            *(short *)(RSP.DMEM + addr - 0x002) = VR[vt][00];
-            *(short *)(RSP.DMEM + addr + 0x004) = VR[vt][01];
-            *(short *)(RSP.DMEM + addr + 0x002) = VR[vt][02];
-            *(short *)(RSP.DMEM + addr + 0x008) = VR[vt][03];
-            *(short *)(RSP.DMEM + addr + 0x006) = VR[vt][04];
+            *(short *)(RSP.DMEM + addr + (0x006 ^ 02)) = VR[vt][00];
+            *(short *)(RSP.DMEM + addr + (0x008 ^ 02)) = VR[vt][01];
+            *(short *)(RSP.DMEM + addr + (0x00A ^ 02)) = VR[vt][02];
+            *(short *)(RSP.DMEM + addr + (0x00C ^ 02)) = VR[vt][03];
+            *(short *)(RSP.DMEM + addr + (0x00E ^ 02)) = VR[vt][04];
             return;
         default:
             message("SQV\nWeird addr.", 3);
             return;
     }
-bitch:
+ILLEGAL:
     message("SQV\nIllegal element.", 3);
     return;
 }
