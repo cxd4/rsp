46e9bef4d1f21e79eeece093985858cb3d4c9b1d
RJ Swedlow <rj150@hotmail.com>
2013-04-09 00:08:54 -0500

    test ROM lookup fixing 11-bit-FP-precise vector divides

--- vu/vrcp.h
+++ vu/vrcp.h
@@ -3,62 +3,32 @@
 
 static void VRCP(int vd, int de, int vt, int e)
 {
-    register int i;
-#ifdef FP_CORRECTIONS
-    unsigned int old_model;
-#endif
-    register int rec;
+    unsigned int addr;
+    int data;
+    int fetch;
+    int shift;
 
     DivIn = (int)VR[vt][e & 07];
-    rec = DivIn;
-    if (rec == 0)
-    { // divide by zero -> overflow
-        rec = 0x7FFFFFFF;
-    }
-    else
-    {
-        int negative = 0;
-        if (rec < 0)
-        {
-            rec = ~rec; /* NOT(rec) followed by INC(rec), ... */
-            ++rec; /* ... I believe is the same as just NEG(rec) ? */
-            negative = 1;
-        }
-/* Old rounding code used in older zilmar RSP plugins.
-        for (i = 15; i > 0; i--)
-        {
-            if (rec & (1 << i))
-            {
-                rec &= ((0xffc0) >> (15 - i));
-                i = 0;
-            }
-        }
-*/
-#ifdef FP_CORRECTIONS
-        old_model = _controlfp(_RC_CHOP, _MCW_RC);
-#endif
-        rec = (signed int)(0x7FFFFFFF / (double)rec);
-#ifdef FP_CORRECTIONS
-        old_model = _controlfp(old_model, _MCW_RC);
-#endif
-/* I am not sure what the newer method is exactly, but this is used for sqrt.
-        for (i = 31; i > 0; i--)
-        {
-            if (rec & (1 << i))
-            {
-                rec &= ((0xffff8000) >> (31 - i));
-                i = 0;
-            }
-        }
-*/
-        if (negative)
-        {
-            rec = ~rec;
-        }
-    }
-    for (i = 0; i < 8; i++)
-        VACC[i].s[LO] = VR[vt][ei[e][i]];
-    DivOut = rec;
+    data = DivIn;
+    if (data < 0)
+        data = -data;
+    for (shift = 0; shift < 32; shift++)
+        if (data & (1 << (shift ^ 31)))
+            break;
+    if (data == 0)
+        shift = 0x10;
+    addr = (data << shift) >> 22;
+    fetch = div_ROM[addr &= 0x000001FF];
+    shift ^= 31; /* For shift &= 31, ^= 31 to flip left- to right-shift. */
+    DivOut = (0x40000000 | (fetch << 14)) >> shift;
+    if (DivIn < 0)
+        DivOut = ~DivOut;
+    if (DivIn == 0) /* corner case:  overflow via division by zero */
+        DivOut = 0x7FFFFFFF;
+    if (DivIn == -32768) /* corner case:  signed underflow barrier */
+        DivOut = 0xFFFF0000;
+    for (shift = 0; shift < 8; shift++)
+        VACC[shift].s[LO] = VR[vt][ei[e][shift]];
     VR[vd][de &= 07] = (short)DivOut;
     DPH = 0;
     return;
