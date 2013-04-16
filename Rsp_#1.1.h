d465db3a728631f4eea27112f0a3e8c9048e4ef1
RJ Swedlow <rj150@hotmail.com>
2013-04-15 22:26:57 -0500

    meh...stupid tab spaces

--- Rsp_#1.1.h
+++ Rsp_#1.1.h
@@ -18,54 +18,53 @@ struct HMENU__ {int unused;};
 typedef struct HMENU__ *HMENU;
 struct HDC__ {int unused;};
 typedef struct HDC__ *HDC;
-#endif
 
 typedef struct {
-	unsigned short Version;        /* Should be set to 0x0101 */
-	unsigned short Type;           /* Set to PLUGIN_TYPE_RSP */
-	char Name[100];      /* Name of the DLL */
+    unsigned short Version;        /* Should be set to 0x0101 */
+    unsigned short Type;           /* Set to PLUGIN_TYPE_RSP */
+    char Name[100];      /* Name of the DLL */
 
-	/* If DLL supports memory these memory options then set them to TRUE or FALSE
-	   if it does not support it */
-	int NormalMemory;   /* a normal BYTE array */ 
-	int MemoryBswaped;  /* a normal BYTE array where the memory has been pre
-	                          bswap on a dword (32 bits) boundry */
+    /* If DLL supports memory these memory options then set them to TRUE or FALSE
+       if it does not support it */
+    int NormalMemory;   /* a normal BYTE array */ 
+    int MemoryBswaped;  /* a normal BYTE array where the memory has been pre
+                              bswap on a dword (32 bits) boundry */
 } PLUGIN_INFO;
 
 typedef struct {
-	HINSTANCE hInst;
-	int MemoryBswaped;    /* If this is set to TRUE, then the memory has been pre
-	                          bswap on a dword (32 bits) boundry */
-	unsigned char *RDRAM;
-	unsigned char *DMEM;
-	unsigned char *IMEM;
+    HINSTANCE hInst;
+    int MemoryBswaped;    /* If this is set to TRUE, then the memory has been pre
+                              bswap on a dword (32 bits) boundry */
+    unsigned char *RDRAM;
+    unsigned char *DMEM;
+    unsigned char *IMEM;
 
-	unsigned long *MI_INTR_REG;
+    unsigned long *MI_INTR_REG;
 
-	unsigned long *SP_MEM_ADDR_REG;
-	unsigned long *SP_DRAM_ADDR_REG;
-	unsigned long *SP_RD_LEN_REG;
-	unsigned long *SP_WR_LEN_REG;
-	unsigned long *SP_STATUS_REG;
-	unsigned long *SP_DMA_FULL_REG;
-	unsigned long *SP_DMA_BUSY_REG;
-	unsigned long *SP_PC_REG;
-	unsigned long *SP_SEMAPHORE_REG;
+    unsigned long *SP_MEM_ADDR_REG;
+    unsigned long *SP_DRAM_ADDR_REG;
+    unsigned long *SP_RD_LEN_REG;
+    unsigned long *SP_WR_LEN_REG;
+    unsigned long *SP_STATUS_REG;
+    unsigned long *SP_DMA_FULL_REG;
+    unsigned long *SP_DMA_BUSY_REG;
+    unsigned long *SP_PC_REG;
+    unsigned long *SP_SEMAPHORE_REG;
 
-	unsigned long *DPC_START_REG;
-	unsigned long *DPC_END_REG;
-	unsigned long *DPC_CURRENT_REG;
-	unsigned long *DPC_STATUS_REG;
-	unsigned long *DPC_CLOCK_REG;
-	unsigned long *DPC_BUFBUSY_REG;
-	unsigned long *DPC_PIPEBUSY_REG;
-	unsigned long *DPC_TMEM_REG;
+    unsigned long *DPC_START_REG;
+    unsigned long *DPC_END_REG;
+    unsigned long *DPC_CURRENT_REG;
+    unsigned long *DPC_STATUS_REG;
+    unsigned long *DPC_CLOCK_REG;
+    unsigned long *DPC_BUFBUSY_REG;
+    unsigned long *DPC_PIPEBUSY_REG;
+    unsigned long *DPC_TMEM_REG;
 
-	void (*CheckInterrupts)( void );
-	void (*ProcessDList)( void );
-	void (*ProcessAList)( void );
-	void (*ProcessRdpList)( void );
-	void (*ShowCFB)( void );
+    void (*CheckInterrupts)( void );
+    void (*ProcessDList)( void );
+    void (*ProcessAList)( void );
+    void (*ProcessRdpList)( void );
+    void (*ShowCFB)( void );
 } RSP_INFO;
 
 typedef struct tagRECT {
@@ -85,37 +84,37 @@ typedef struct tagPAINTSTRUCT {
 } PAINTSTRUCT;
 
 typedef struct {
-	/* Menu */
-	/* Items should have an ID between 5001 and 5100 */
-	HMENU hRSPMenu;
-	void (*ProcessMenuItem) ( int ID );
+    /* Menu */
+    /* Items should have an ID between 5001 and 5100 */
+    HMENU hRSPMenu;
+    void (*ProcessMenuItem) ( int ID );
 
-	/* Break Points */
-	int UseBPoints;
-	char BPPanelName[20];
-	void (*Add_BPoint)     (void);
-	void (*CreateBPPanel)  (HWND hDlg, RECT rcBox);
-	void (*HideBPPanel)    (void);
-	void (*PaintBPPanel)   (PAINTSTRUCT ps);
-	void (*ShowBPPanel)    (void);
-	void (*RefreshBpoints) (HWND hList);
-	void (*RemoveBpoint)   (HWND hList, int index);
-	void (*RemoveAllBpoint)(void);
-	
-	/* RSP command Window */
-	void (*Enter_RSP_Commands_Window)(void);
+    /* Break Points */
+    int UseBPoints;
+    char BPPanelName[20];
+    void (*Add_BPoint)     (void);
+    void (*CreateBPPanel)  (HWND hDlg, RECT rcBox);
+    void (*HideBPPanel)    (void);
+    void (*PaintBPPanel)   (PAINTSTRUCT ps);
+    void (*ShowBPPanel)    (void);
+    void (*RefreshBpoints) (HWND hList);
+    void (*RemoveBpoint)   (HWND hList, int index);
+    void (*RemoveAllBpoint)(void);
+    
+    /* RSP command Window */
+    void (*Enter_RSP_Commands_Window)(void);
 
 } RSPDEBUG_INFO;
 
 typedef struct {
-	void (*UpdateBreakPoints)(void);
-	void (*UpdateMemory)(void);
-	void (*UpdateR4300iRegisters)(void);
-	void (*Enter_BPoint_Window)(void);
-	void (*Enter_R4300i_Commands_Window)(void);
-	void (*Enter_R4300i_Register_Window)(void);
-	void (*Enter_RSP_Commands_Window) (void);
-	void (*Enter_Memory_Window)(void);
+    void (*UpdateBreakPoints)(void);
+    void (*UpdateMemory)(void);
+    void (*UpdateR4300iRegisters)(void);
+    void (*Enter_BPoint_Window)(void);
+    void (*Enter_R4300i_Commands_Window)(void);
+    void (*Enter_R4300i_Register_Window)(void);
+    void (*Enter_RSP_Commands_Window) (void);
+    void (*Enter_Memory_Window)(void);
 } DEBUG_INFO;
 
 #if defined(WIN32)
@@ -166,12 +165,12 @@ EXPORT void CALL DllTest(HWND hParent);
   Function: DoRspCycles
   Purpose:  This function is to allow the RSP to run in parrel with
             the r4300 switching control back to the r4300 once the
-			function ends.
+            function ends.
   input:    The number of cylces that is meant to be executed
   output:   The number of cycles that was executed. This value can
             be greater than the number of cycles that the RSP 
-			should have performed.
-			(this value is ignored if the RSP is stoped)
+            should have performed.
+            (this value is ignored if the RSP is stoped)
 *******************************************************************/ 
 EXPORT unsigned int CALL DoRspCycles(unsigned int Cycles);
 
@@ -189,7 +188,7 @@ EXPORT void CALL GetDllInfo(PLUGIN_INFO *PluginInfo);
   Function: GetRspDebugInfo
   Purpose:  This function allows the emulator to gather information
             about the debug capabilities of the dll by filling in
-			the DebugInfo structure.
+            the DebugInfo structure.
   input:    a pointer to a RSPDEBUG_INFO stucture that needs to be
             filled by the function. (see def above)
   output:   none
@@ -200,11 +199,11 @@ EXPORT void CALL GetRspDebugInfo(RSPDEBUG_INFO *RSPDebugInfo);
   Function: InitiateRSP
   Purpose:  This function is called when the DLL is started to give
             information from the emulator that the n64 RSP 
-			interface needs
+            interface needs
   input:    Rsp_Info is passed to this function which is defined
             above.
-			CycleCount is the number of cycles between switching
-			control between the RSP and r4300i core.
+            CycleCount is the number of cycles between switching
+            control between the RSP and r4300i core.
   output:   none
 *******************************************************************/ 
 EXPORT void CALL InitiateRSP(RSP_INFO Rsp_Info, unsigned int *CycleCount);
@@ -213,8 +212,8 @@ EXPORT void CALL InitiateRSP(RSP_INFO Rsp_Info, unsigned int *CycleCount);
   Function: InitiateRSPDebugger
   Purpose:  This function is called when the DLL is started to give
             information from the emulator that the n64 RSP 
-			interface needs to intergrate the debugger with the
-			rest of the emulator.
+            interface needs to intergrate the debugger with the
+            rest of the emulator.
   input:    DebugInfo is passed to this function which is defined
             above.
   output:   none
