864be33c85d9731f68eea4a5ac1dab3c0e7b1ae7
RJ Swedlow <rj150@hotmail.com>
2013-04-19 20:26:34 -0500

    support mid-segment crosses (All-Star Baseball '99 players)

--- su/lwc2/llv.h
+++ su/lwc2/llv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Load Longword to Vector Unit               *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.03.21                                                         *
+* Release:  2013.04.19                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -9,7 +9,7 @@ void LLV(int vt, int element, signed int offset, int base)
 {
     register unsigned int addr;
 
-    addr  = SR[base] + (offset << 2);
+    addr  = SR[base] + (offset <<= 2);
     addr &= 0x00000FFF;
     switch (addr & 03)
     {
@@ -17,10 +17,15 @@ void LLV(int vt, int element, signed int offset, int base)
             VR_S(vt, element+0x0) = *(short *)(RSP.DMEM + addr + (0x000 ^ 02));
             VR_S(vt, element+0x2) = *(short *)(RSP.DMEM + addr + (0x002 ^ 02));
             return;
+        case 02: /* F3DLX 1.23:  "All-Star Baseball '99" */
+            VR_S(vt, element+0x0) = *(short *)(RSP.DMEM + addr - (0x000 ^ 02));
+            addr += 0x002 + 02; /* halfword endian swap adjust */
+            addr &= 0x00000FFF;
+            VR_S(vt, element+0x2) = *(short *)(RSP.DMEM + addr);
+            return;
         case 01:
-        case 02:
         case 03:
-            message("LLV\nWeird addr.", 3);
+            message("LLV\nOdd addr.", 3);
             return;
     }
 }
