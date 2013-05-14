11c1c71a4fd8c7410c57186f22ccc5522b6a14e7
RJ Swedlow <rj150@hotmail.com>
2013-05-14 12:35:51 -0400

    support strange CFB-effect game in "Doubutsu no Mori"

--- su/lwc2/lpv.h
+++ su/lwc2/lpv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Load Packed Signed to Vector Unit          *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.04.20                                                         *
+* Release:  2013.05.14                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -13,9 +13,9 @@ void LPV(int vt, int element, signed int offset, int base)
     if (element != 0x0)
         goto ILLEGAL_EL;
     addr  = SR[base] + (offset <<= 3);
-    b = addr & 0x00000007;
+    b = addr;
     addr &= 0x00000FF8;
-    switch (b)
+    switch (b &= 07)
     {
         case 00:
             VR[vt][07] = RSP.DMEM[addr + BES(0x007)] << 8;
@@ -27,7 +27,43 @@ void LPV(int vt, int element, signed int offset, int base)
             VR[vt][01] = RSP.DMEM[addr + BES(0x001)] << 8;
             VR[vt][00] = RSP.DMEM[addr + BES(0x000)] << 8;
             return;
-        case 04: /* WWF No Mercy F3DLX 2.08, Resident Evil 2 in-game 3-D */
+        case 01: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            VR[vt][00] = RSP.DMEM[addr + BES(0x001)] << 8;
+            VR[vt][01] = RSP.DMEM[addr + BES(0x002)] << 8;
+            VR[vt][02] = RSP.DMEM[addr + BES(0x003)] << 8;
+            VR[vt][03] = RSP.DMEM[addr + BES(0x004)] << 8;
+            VR[vt][04] = RSP.DMEM[addr + BES(0x005)] << 8;
+            VR[vt][05] = RSP.DMEM[addr + BES(0x006)] << 8;
+            VR[vt][06] = RSP.DMEM[addr + BES(0x007)] << 8;
+            addr += BES(0x008);
+            addr &= 0x00000FFF;
+            VR[vt][07] = RSP.DMEM[addr] << 8;
+            return;
+        case 02: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            VR[vt][00] = RSP.DMEM[addr + BES(0x002)] << 8;
+            VR[vt][01] = RSP.DMEM[addr + BES(0x003)] << 8;
+            VR[vt][02] = RSP.DMEM[addr + BES(0x004)] << 8;
+            VR[vt][03] = RSP.DMEM[addr + BES(0x005)] << 8;
+            VR[vt][04] = RSP.DMEM[addr + BES(0x006)] << 8;
+            VR[vt][05] = RSP.DMEM[addr + BES(0x007)] << 8;
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            VR[vt][06] = RSP.DMEM[addr + BES(0x000)] << 8;
+            VR[vt][07] = RSP.DMEM[addr + BES(0x001)] << 8;
+            return;
+        case 03: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            VR[vt][00] = RSP.DMEM[addr + BES(0x003)] << 8;
+            VR[vt][01] = RSP.DMEM[addr + BES(0x004)] << 8;
+            VR[vt][02] = RSP.DMEM[addr + BES(0x005)] << 8;
+            VR[vt][03] = RSP.DMEM[addr + BES(0x006)] << 8;
+            VR[vt][04] = RSP.DMEM[addr + BES(0x007)] << 8;
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            VR[vt][05] = RSP.DMEM[addr + BES(0x000)] << 8;
+            VR[vt][06] = RSP.DMEM[addr + BES(0x001)] << 8;
+            VR[vt][07] = RSP.DMEM[addr + BES(0x002)] << 8;
+            return;
+        case 04: /* F3DLX 2.08 "WWF No Mercy", Resident Evil 2 in-game 3-D */
             VR[vt][00] = RSP.DMEM[addr + BES(0x004)] << 8;
             VR[vt][01] = RSP.DMEM[addr + BES(0x005)] << 8;
             VR[vt][02] = RSP.DMEM[addr + BES(0x006)] << 8;
@@ -39,8 +75,41 @@ void LPV(int vt, int element, signed int offset, int base)
             VR[vt][06] = RSP.DMEM[addr + BES(0x002)] << 8;
             VR[vt][07] = RSP.DMEM[addr + BES(0x003)] << 8;
             return;
-        default: /* Completely legal, just never seen it be done. */
-            message("LPV\nWeird addr.", 3);
+        case 05: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            VR[vt][00] = RSP.DMEM[addr + BES(0x005)] << 8;
+            VR[vt][01] = RSP.DMEM[addr + BES(0x006)] << 8;
+            VR[vt][02] = RSP.DMEM[addr + BES(0x007)] << 8;
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            VR[vt][03] = RSP.DMEM[addr + BES(0x000)] << 8;
+            VR[vt][04] = RSP.DMEM[addr + BES(0x001)] << 8;
+            VR[vt][05] = RSP.DMEM[addr + BES(0x002)] << 8;
+            VR[vt][06] = RSP.DMEM[addr + BES(0x003)] << 8;
+            VR[vt][07] = RSP.DMEM[addr + BES(0x004)] << 8;
+            return;
+        case 06: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            VR[vt][00] = RSP.DMEM[addr + BES(0x006)] << 8;
+            VR[vt][01] = RSP.DMEM[addr + BES(0x007)] << 8;
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            VR[vt][02] = RSP.DMEM[addr + BES(0x000)] << 8;
+            VR[vt][03] = RSP.DMEM[addr + BES(0x001)] << 8;
+            VR[vt][04] = RSP.DMEM[addr + BES(0x002)] << 8;
+            VR[vt][05] = RSP.DMEM[addr + BES(0x003)] << 8;
+            VR[vt][06] = RSP.DMEM[addr + BES(0x004)] << 8;
+            VR[vt][07] = RSP.DMEM[addr + BES(0x005)] << 8;
+            return;
+        case 07: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            VR[vt][00] = RSP.DMEM[addr + BES(0x007)] << 8;
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            VR[vt][01] = RSP.DMEM[addr + BES(0x000)] << 8;
+            VR[vt][02] = RSP.DMEM[addr + BES(0x001)] << 8;
+            VR[vt][03] = RSP.DMEM[addr + BES(0x002)] << 8;
+            VR[vt][04] = RSP.DMEM[addr + BES(0x003)] << 8;
+            VR[vt][05] = RSP.DMEM[addr + BES(0x004)] << 8;
+            VR[vt][06] = RSP.DMEM[addr + BES(0x005)] << 8;
+            VR[vt][07] = RSP.DMEM[addr + BES(0x006)] << 8;
             return;
     }
 ILLEGAL_EL:
--- su/swc2/spv.h
+++ su/swc2/spv.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  SP VU Emulation Table:  Store Packed Signed from Vector Unit       *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.04.20                                                         *
+* Release:  2013.05.14                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 
@@ -13,9 +13,9 @@ void SPV(int vt, int element, signed int offset, int base)
     if (element != 0x0)
         goto ILLEGAL_EL;
     addr  = SR[base] + (offset <<= 3);
-    b = addr & 0x00000007;
+    b = addr;
     addr &= 0x00000FF8;
-    switch (b)
+    switch (b &= 07)
     {
         case 00:
             RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][07] >> 8);
@@ -27,8 +27,89 @@ void SPV(int vt, int element, signed int offset, int base)
             RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][01] >> 8);
             RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][00] >> 8);
             return;
-        default: /* Completely legal, just never seen it be done. */
-            message("SPV\nWeird addr.", 3);
+        case 01: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][00] >> 8);
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][01] >> 8);
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][02] >> 8);
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][03] >> 8);
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][04] >> 8);
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][05] >> 8);
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][06] >> 8);
+            addr += BES(0x008);
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr] = (unsigned char)(VR[vt][07] >> 8);
+            return;
+        case 02: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][00] >> 8);
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][01] >> 8);
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][02] >> 8);
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][03] >> 8);
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][04] >> 8);
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][05] >> 8);
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][06] >> 8);
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][07] >> 8);
+            return;
+        case 03: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][00] >> 8);
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][01] >> 8);
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][02] >> 8);
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][03] >> 8);
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][04] >> 8);
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][05] >> 8);
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][06] >> 8);
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][07] >> 8);
+            return;
+        case 04: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][00] >> 8);
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][01] >> 8);
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][02] >> 8);
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][03] >> 8);
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][04] >> 8);
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][05] >> 8);
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][06] >> 8);
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][07] >> 8);
+            return;
+        case 05: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][00] >> 8);
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][01] >> 8);
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][02] >> 8);
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][03] >> 8);
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][04] >> 8);
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][05] >> 8);
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][06] >> 8);
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][07] >> 8);
+            return;
+        case 06: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][00] >> 8);
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][01] >> 8);
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][02] >> 8);
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][03] >> 8);
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][04] >> 8);
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][05] >> 8);
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][06] >> 8);
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][07] >> 8);
+            return;
+        case 07: /* F3DZEX 2.08J "Doubutsu no Mori" (Animal Forest) CFB layer */
+            RSP.DMEM[addr + BES(0x007)] = (unsigned char)(VR[vt][00] >> 8);
+            addr += 0x008;
+            addr &= 0x00000FFF;
+            RSP.DMEM[addr + BES(0x000)] = (unsigned char)(VR[vt][01] >> 8);
+            RSP.DMEM[addr + BES(0x001)] = (unsigned char)(VR[vt][02] >> 8);
+            RSP.DMEM[addr + BES(0x002)] = (unsigned char)(VR[vt][03] >> 8);
+            RSP.DMEM[addr + BES(0x003)] = (unsigned char)(VR[vt][04] >> 8);
+            RSP.DMEM[addr + BES(0x004)] = (unsigned char)(VR[vt][05] >> 8);
+            RSP.DMEM[addr + BES(0x005)] = (unsigned char)(VR[vt][06] >> 8);
+            RSP.DMEM[addr + BES(0x006)] = (unsigned char)(VR[vt][07] >> 8);
             return;
     }
 ILLEGAL_EL:
