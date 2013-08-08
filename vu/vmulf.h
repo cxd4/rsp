a635542af6d94ffe743034a8011824a95fb66b2f
RJ Swedlow <rj150@hotmail.com>
2013-08-08 18:07:56 -0400

    discovered an old SSE intrinsic for corner case

--- vu/vmulf.h
+++ vu/vmulf.h
@@ -6,7 +6,9 @@ static void VMULF(int vd, int vs, int vt, int e)
 
     for (i = 0; i < 8; i++)
         VACC[i].DW = (VR[vs][i]*VR_T(i) << 1) + 0x8000;
+    for (i = 0; i < 8; i++)
+        VR_D(i) = VACC[i].s[MD];
     for (i = 0; i < 8; i++) /* Only one reachable value can expose overflow. */
-        VR_D(i) = VACC[i].s[MD] + (signed short)(VACC[i].s[MD] >> 15);
+        VR_D(i) -= !!(VR_D(i) & 0x8000);
     return;
 }
