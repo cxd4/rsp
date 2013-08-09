e87066933e3e83fa0cb30c5040df080de0da121e
RJ Swedlow <rj150@hotmail.com>
2013-08-08 22:52:19 -0400

    uh, forgot the sperm

--- vu/vaddc.h
+++ vu/vaddc.h
@@ -11,7 +11,7 @@ static void VADDC(int vd, int vs, int vt, int e)
     for (i = 0; i < 8; i++)
         ACC_R(i) = (short)result[i];
     for (i = 0; i < 8; i++)
-        VCO |= !!(result[i] & 0x0000FFFF) << i;
+        VCO |= !!(result[i] & ~0x0000FFFF) << i;
     for (i = 0; i < 8; i++)
         ACC_W(i) = ACC_R(i);
     return;
