41819a945050ef73174c36bdb893b0b3a92f0db3
RJ Swedlow <rj150@hotmail.com>
2013-08-27 17:52:06 -0400

    more ANSI warnings about printf in-lined types

--- rsp.h
+++ rsp.h
@@ -167,6 +167,7 @@ void export_SP_memory(void)
 
 void trace_RSP_registers(void)
 { /* no interface--using file I/O only */
+    register int i;
     FILE *out = fopen("SP_STATE.TXT", "w");
 
     fprintf(out, "RCP Communications Register Display\n\n");
@@ -178,23 +179,23 @@ void trace_RSP_registers(void)
  * Whether or not you find these names agreeable is mostly a matter of seeing
  * them from the RCP's point of view or the CPU host's mapped point of view.
  */
-    fprintf(out, "SP_MEM_ADDR:    %08X    CMD_START:      %08X\n",
+    fprintf(out, "SP_MEM_ADDR:    %08lX    CMD_START:      %08lX\n",
         *RSP.SP_MEM_ADDR_REG, *RSP.DPC_START_REG);
-    fprintf(out, "SP_DRAM_ADDR:   %08X    CMD_END:        %08X\n",
+    fprintf(out, "SP_DRAM_ADDR:   %08lX    CMD_END:        %08lX\n",
         *RSP.SP_DRAM_ADDR_REG, *RSP.DPC_END_REG);
-    fprintf(out, "SP_DMA_RD_LEN:  %08X    CMD_CURRENT:    %08X\n",
+    fprintf(out, "SP_DMA_RD_LEN:  %08lX    CMD_CURRENT:    %08lX\n",
         *RSP.SP_RD_LEN_REG, *RSP.DPC_CURRENT_REG);
-    fprintf(out, "SP_DMA_WR_LEN:  %08X    CMD_STATUS:     %08X\n",
+    fprintf(out, "SP_DMA_WR_LEN:  %08lX    CMD_STATUS:     %08lX\n",
         *RSP.SP_WR_LEN_REG, *RSP.DPC_STATUS_REG);
-    fprintf(out, "SP_STATUS:      %08X    CMD_CLOCK:      %08X\n",
+    fprintf(out, "SP_STATUS:      %08lX    CMD_CLOCK:      %08lX\n",
         *RSP.SP_STATUS_REG, *RSP.DPC_CLOCK_REG);
-    fprintf(out, "SP_DMA_FULL:    %08X    CMD_BUSY:       %08X\n",
+    fprintf(out, "SP_DMA_FULL:    %08lX    CMD_BUSY:       %08lX\n",
         *RSP.SP_DMA_FULL_REG, *RSP.DPC_BUFBUSY_REG);
-    fprintf(out, "SP_DMA_BUSY:    %08X    CMD_PIPE_BUSY:  %08X\n",
+    fprintf(out, "SP_DMA_BUSY:    %08lX    CMD_PIPE_BUSY:  %08lX\n",
         *RSP.SP_DMA_BUSY_REG, *RSP.DPC_PIPEBUSY_REG);
-    fprintf(out, "SP_SEMAPHORE:   %08X    CMD_TMEM_BUSY:  %08X\n",
+    fprintf(out, "SP_SEMAPHORE:   %08lX    CMD_TMEM_BUSY:  %08lX\n",
         *RSP.SP_SEMAPHORE_REG, *RSP.DPC_TMEM_REG);
-    fprintf(out, "SP_PC_REG:      04001%03X\n\n", *RSP.SP_PC_REG & 0x00000FFF);
+    fprintf(out, "SP_PC_REG:      04001%03lX\n\n", *RSP.SP_PC_REG & 0x00000FFF);
 /* (PC is only from the CPU point of view, mapped between both halves.) */
 /* There is no memory map for remaining registers not shared by the CPU.
  * The scalar register (SR) file is straightforward and based on the
@@ -218,136 +219,36 @@ void trace_RSP_registers(void)
     fprintf(out, " t3:  %08X,  k1:  %08X,\n", SR[11], SR[27]);
     fprintf(out, " t4:  %08X,  gp:  %08X,\n", SR[12], SR[28]);
     fprintf(out, " t5:  %08X, $sp:  %08X,\n", SR[13], SR[29]);
-    fprintf(out, " t6:  %08X, $fp:  %08X,\n", SR[14], SR[30]);
+    fprintf(out, " t6:  %08X, $s8:  %08X,\n", SR[14], SR[30]);
     fprintf(out, " t7:  %08X, $ra:  %08X\n\n", SR[15], SR[31]);
-/* (Yes, I rebelliously used the MIPS32 "$fp" modern alias over lazy "$s8".) */
-/* The RSP vector registers are incontestedly named $v[n] and nothing else.
- * The problem is organizing the contents by HW/B elements and proper endian.
- */
-    fprintf(out, " $v0:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 0][00], VR[ 0][01], VR[ 0][02], VR[ 0][03],
-        VR[ 0][04], VR[ 0][05], VR[ 0][06], VR[ 0][07]);
-    fprintf(out, " $v1:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 1][00], VR[ 1][01], VR[ 1][02], VR[ 1][03],
-        VR[ 1][04], VR[ 1][05], VR[ 1][06], VR[ 1][07]);
-    fprintf(out, " $v2:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 2][00], VR[ 2][01], VR[ 2][02], VR[ 2][03],
-        VR[ 2][04], VR[ 2][05], VR[ 2][06], VR[ 2][07]);
-    fprintf(out, " $v3:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 3][00], VR[ 3][01], VR[ 3][02], VR[ 3][03],
-        VR[ 3][04], VR[ 3][05], VR[ 3][06], VR[ 3][07]);
-    fprintf(out, " $v4:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 4][00], VR[ 4][01], VR[ 4][02], VR[ 4][03],
-        VR[ 4][04], VR[ 4][05], VR[ 4][06], VR[ 4][07]);
-    fprintf(out, " $v5:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 5][00], VR[ 5][01], VR[ 5][02], VR[ 5][03],
-        VR[ 5][04], VR[ 5][05], VR[ 5][06], VR[ 5][07]);
-    fprintf(out, " $v6:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 6][00], VR[ 6][01], VR[ 6][02], VR[ 6][03],
-        VR[ 6][04], VR[ 6][05], VR[ 6][06], VR[ 6][07]);
-    fprintf(out, " $v7:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 7][00], VR[ 7][01], VR[ 7][02], VR[ 7][03],
-        VR[ 7][04], VR[ 7][05], VR[ 7][06], VR[ 7][07]);
-    fprintf(out, " $v8:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 8][00], VR[ 8][01], VR[ 8][02], VR[ 8][03],
-        VR[ 8][04], VR[ 8][05], VR[ 8][06], VR[ 8][07]);
-    fprintf(out, " $v9:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[ 9][00], VR[ 9][01], VR[ 9][02], VR[ 9][03],
-        VR[ 9][04], VR[ 9][05], VR[ 9][06], VR[ 9][07]);
-    fprintf(out, "$v10:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[10][00], VR[10][01], VR[10][02], VR[10][03],
-        VR[10][04], VR[10][05], VR[10][06], VR[10][07]);
-    fprintf(out, "$v11:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[11][00], VR[11][01], VR[11][02], VR[11][03],
-        VR[11][04], VR[11][05], VR[11][06], VR[11][07]);
-    fprintf(out, "$v12:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[12][00], VR[12][01], VR[12][02], VR[12][03],
-        VR[12][04], VR[12][05], VR[12][06], VR[12][07]);
-    fprintf(out, "$v13:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[13][00], VR[13][01], VR[13][02], VR[13][03],
-        VR[13][04], VR[13][05], VR[13][06], VR[13][07]);
-    fprintf(out, "$v14:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[14][00], VR[14][01], VR[14][02], VR[14][03],
-        VR[14][04], VR[14][05], VR[14][06], VR[14][07]);
-    fprintf(out, "$v15:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[15][00], VR[15][01], VR[15][02], VR[15][03],
-        VR[15][04], VR[15][05], VR[15][06], VR[15][07]);
-    fprintf(out, "$v16:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[16][00], VR[16][01], VR[16][02], VR[16][03],
-        VR[16][04], VR[16][05], VR[16][06], VR[16][07]);
-    fprintf(out, "$v17:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[17][00], VR[17][01], VR[17][02], VR[17][03],
-        VR[17][04], VR[17][05], VR[17][06], VR[17][07]);
-    fprintf(out, "$v18:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[18][00], VR[18][01], VR[18][02], VR[18][03],
-        VR[18][04], VR[18][05], VR[18][06], VR[18][07]);
-    fprintf(out, "$v19:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[19][00], VR[19][01], VR[19][02], VR[19][03],
-        VR[19][04], VR[19][05], VR[19][06], VR[19][07]);
-    fprintf(out, "$v20:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[20][00], VR[20][01], VR[20][02], VR[20][03],
-        VR[20][04], VR[20][05], VR[20][06], VR[20][07]);
-    fprintf(out, "$v21:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[21][00], VR[21][01], VR[21][02], VR[21][03],
-        VR[21][04], VR[21][05], VR[21][06], VR[21][07]);
-    fprintf(out, "$v22:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[22][00], VR[22][01], VR[22][02], VR[22][03],
-        VR[22][04], VR[22][05], VR[22][06], VR[22][07]);
-    fprintf(out, "$v23:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[23][00], VR[23][01], VR[23][02], VR[23][03],
-        VR[23][04], VR[23][05], VR[23][06], VR[23][07]);
-    fprintf(out, "$v24:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[24][00], VR[24][01], VR[24][02], VR[24][03],
-        VR[24][04], VR[24][05], VR[24][06], VR[24][07]);
-    fprintf(out, "$v25:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[25][00], VR[25][01], VR[25][02], VR[25][03],
-        VR[25][04], VR[25][05], VR[25][06], VR[25][07]);
-    fprintf(out, "$v26:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[26][00], VR[26][01], VR[26][02], VR[26][03],
-        VR[26][04], VR[26][05], VR[26][06], VR[26][07]);
-    fprintf(out, "$v27:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[27][00], VR[27][01], VR[27][02], VR[27][03],
-        VR[27][04], VR[27][05], VR[27][06], VR[27][07]);
-    fprintf(out, "$v28:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[28][00], VR[28][01], VR[28][02], VR[28][03],
-        VR[28][04], VR[28][05], VR[28][06], VR[28][07]);
-    fprintf(out, "$v29:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[29][00], VR[29][01], VR[29][02], VR[29][03],
-        VR[29][04], VR[29][05], VR[29][06], VR[29][07]);
-    fprintf(out, "$v30:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n",
-        VR[30][00], VR[30][01], VR[30][02], VR[30][03],
-        VR[30][04], VR[30][05], VR[30][06], VR[30][07]);
-    fprintf(out, "$v31:  [%04X][%04X][%04X][%04X][%04X][%04X][%04X][%04X]\n\n"
-      , VR[31][00], VR[31][01], VR[31][02], VR[31][03],
-        VR[31][04], VR[31][05], VR[31][06], VR[31][07]);
-/* The SU has its fair share of registers, but the VU has its counterparts.
+
+    for (i = 0; i < 10; i++)
+        fprintf(
+            out,
+           " $v%i:  [%04hX][%04hX][%04hX][%04hX][%04hX][%04hX][%04hX][%04hX]\n",
+            VR[i][00], VR[i][01], VR[i][02], VR[i][03],
+            VR[i][04], VR[i][05], VR[i][06], VR[i][07]);
+    for (i = 10; i < 32; i++) /* decimals "10" and higher with two characters */
+        fprintf(
+            out,
+            "$v%i:  [%04hX][%04hX][%04hX][%04hX][%04hX][%04hX][%04hX][%04hX]\n",
+            VR[i][00], VR[i][01], VR[i][02], VR[i][03],
+            VR[i][04], VR[i][05], VR[i][06], VR[i][07]);
+    fprintf(out, "\n");
+/*
+ * The SU has its fair share of registers, but the VU has its counterparts.
  * Just like we have the scalar 16 system control registers for the RSP CP0,
  * we have also a tiny group of special-purpose, vector control registers.
  */
     fprintf(out, "$vco:  [%02X][%02X]\n", (VCO >> 8), VCO & 0x00FF);
     fprintf(out, "$vcc:  [%02X][%02X]\n", (VCC >> 8), VCC & 0x00FF);
     fprintf(out, "$vce:  %02X\n\n", VCE); /* 8-bit vector cnd. flags register */
-/* Last and least are the 48-bit accumulator elements, which literally may or
- * may not be considered as "registers" but are still useful to debug, in
- * spite of how near-pointless emulating them is (mostly VSAR/VSAW, VMAC*).
- *
- * I have not confirmed memory endianness controversies for ordering bytes.
- * I decided, better to print 48 bits, not 3 pairs of 16, cause basically
- * every write to the accumulator updates potentially all 48 bits, not 1 HW.
- * ("%012X" seems a better idea than [%04X][%04X][%04X], similar to above.)
- * Without VSAR/VSAW, all single HW R/W's are the low 16 bits of acc. only.
- */
-    fprintf(out, "VACC[0]:  %012X\n", VACC[00].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[1]:  %012X\n", VACC[01].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[2]:  %012X\n", VACC[02].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[3]:  %012X\n", VACC[03].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[4]:  %012X\n", VACC[04].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[5]:  %012X\n", VACC[05].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[6]:  %012X\n", VACC[06].DW & 0xFFFFFFFFFFFF);
-    fprintf(out, "VACC[7]:  %012X\n\n", VACC[07].DW & 0xFFFFFFFFFFFF);
-/* I lied (sort of).  Can't possibly forget the internal registers used by
- * the computational vector divide operations!  (Hmmm okay now I lied.)
- */
+
+    for (i = 0; i < 8; i++)
+        fprintf(
+            out, "ACC[%o]:  [%04X][%04X][%04X]\n", i,
+            VACC[i].s[HI], VACC[i].s[MD], VACC[i].s[LO]);
+    fprintf(out, "\n");
     fprintf(out, "DivIn:  %i\n", DivIn);
     fprintf(out, "DivOut:  %i\n", DivOut);
     /* MovIn:  This reg. might exist for VMOV, but it is obsolete to emulate. */
