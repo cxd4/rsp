d8d3c7ce90be5fbe8f9d4c9d72a449fb4554152d
unknown <rj150@hotmail.com>
2013-09-16 02:18:54 -0400

    in-depth vector microoptimizations to VMUDN SSE

--- vu/vmudn.h
+++ vu/vmudn.h
@@ -2,12 +2,17 @@
 
 INLINE void do_mudn(short* VD, short* VS, short* VT)
 {
-    INT64 acc[N];
+    long acc[N];
     register int i;
 
     for (i = 0; i < N; i++)
         acc[i] = (unsigned short)(VS[i]) * (signed short)(VT[i]);
-    do_store(acc);
+    for (i = 0; i < N; i++)
+        ACC_L(i) = (acc[i] & 0x00000000FFFF) >>  0;
+    for (i = 0; i < N; i++)
+        ACC_M(i) = (acc[i] & 0x0000FFFF0000) >> 16;
+    for (i = 0; i < N; i++)
+        ACC_H(i) = -(ACC_M(i) < 0); /* internal sign-extended shift */
     for (i = 0; i < N; i++)
         VD[i] = ACC_L(i); /* no possibilities to clamp */
     return;
