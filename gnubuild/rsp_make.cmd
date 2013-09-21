f316819f5a05d906f95b4cd6e18a98799c0258d7
Tyler Stachecki <tstache1@cs.binghamton.edu>
2013-09-21 01:51:43 -0400

    Do not pass -m3dnow to gcc.
    The 3DNow! instruction set is not supported by Intel processors.
    Although gcc will not likely emit 3DNow! instructions, it is
    dangerous to permit 3DNow! instructions due to the fact that Intel
    has never supported 3DNow! instructions.

--- gnubuild/rsp_make.cmd
+++ gnubuild/rsp_make.cmd
@@ -3,21 +3,21 @@ TITLE MinGW Compiler Suite Invocation
 CD ..\..\MINGW\BIN\
 
 ECHO Building RSP interpreter... (LLE gfx, LLE audio)
-gcc --shared -s -O3 -m3dnow -mmmx -msse -msse2 -o ../rsp/rsp.dll ../../rsp/rsp.c
+gcc --shared -s -O3 -mmmx -msse -msse2 -o ../rsp/rsp.dll ../../rsp/rsp.c
 
 ECHO Building RSP interpreter... (LLE gfx, HLE audio)
-gcc --shared -s -O3 -m3dnow -mmmx -msse -msse2 -DEXTERN_COMMAND_LIST_ABI -o ../rsp/rsp_lle.dll ../../rsp/rsp.c
+gcc --shared -s -O3 -mmmx -msse -msse2 -DEXTERN_COMMAND_LIST_ABI -o ../rsp/rsp_lle.dll ../../rsp/rsp.c
 
 ECHO Building RSP interpreter... (HLE gfx, LLE audio)
-gcc --shared -s -O3 -m3dnow -mmmx -msse -msse2 -DEXTERN_COMMAND_LIST_GBI -o ../rsp/rsp_mle.dll ../../rsp/rsp.c
+gcc --shared -s -O3 -mmmx -msse -msse2 -DEXTERN_COMMAND_LIST_GBI -o ../rsp/rsp_mle.dll ../../rsp/rsp.c
 
 ECHO Building RSP interpreter... (HLE gfx, HLE audio)
-gcc --shared -s -O3 -m3dnow -mmmx -msse -msse2 -DEXTERN_COMMAND_LIST_GBI -DEXTERN_COMMAND_LIST_ABI -o ../rsp/rsp_hle.dll ../../rsp/rsp.c
+gcc --shared -s -O3 -mmmx -msse -msse2 -DEXTERN_COMMAND_LIST_GBI -DEXTERN_COMMAND_LIST_ABI -o ../rsp/rsp_hle.dll ../../rsp/rsp.c
 
 ECHO Building RSP interpreter... (need Project64 2.x)
-gcc --shared -s -O3 -m3dnow -mmmx -msse -msse2 -DSEMAPHORE_LOCK_CORRECTIONS -DWAIT_FOR_CPU_HOST -o ../rsp/rsp_pj64.dll ../../rsp/rsp.c
+gcc --shared -s -O3 -mmmx -msse -msse2 -DSEMAPHORE_LOCK_CORRECTIONS -DWAIT_FOR_CPU_HOST -o ../rsp/rsp_pj64.dll ../../rsp/rsp.c
 
 ECHO Building RSP interpreter... (debugging module)
-gcc --shared -s -O3 -m3dnow -mmmx -msse -msse2 -DSP_EXECUTE_LOG -DVU_EMULATE_SCALAR_ACCUMULATOR_READ -o ../rsp/rsp_dbg.dll ../../rsp/rsp.c
+gcc --shared -s -O3 -mmmx -msse -msse2 -DSP_EXECUTE_LOG -DVU_EMULATE_SCALAR_ACCUMULATOR_READ -o ../rsp/rsp_dbg.dll ../../rsp/rsp.c
 
 PAUSE
