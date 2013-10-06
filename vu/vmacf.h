38a03b25664dc4daf5c62c34457b5542fff657f8
unknown <rj150@hotmail.com>
2013-10-06 09:49:43 -0400

    found similar fraction delay speed-ups from MUL as with MAC

--- vu/vmacf.h
+++ vu/vmacf.h
@@ -9,21 +9,19 @@ INLINE static void do_macf(short* VD, short* VS, short* VT)
     for (i = 0; i < N; i++)
         product[i] = VS[i] * VT[i];
     for (i = 0; i < N; i++)
-        VACC_H[i] -= (product[i] < 0);
-    for (i = 0; i < N; i++)
-        product[i] = product[i] << 1;
-    for (i = 0; i < N; i++)
-        addend[i] = (product[i] & 0x00000000FFFF) >>  0;
+        addend[i] = (product[i] << 1) & 0x00000000FFFF;
     for (i = 0; i < N; i++)
         addend[i] = (unsigned short)(VACC_L[i]) + addend[i];
     for (i = 0; i < N; i++)
         VACC_L[i] = (short)(addend[i]);
     for (i = 0; i < N; i++)
-        addend[i] = (addend[i] >> 16) + (unsigned short)(product[i] >> 16);
+        addend[i] = (addend[i] >> 16) + (unsigned short)(product[i] >> 15);
     for (i = 0; i < N; i++)
         addend[i] = (unsigned short)(VACC_M[i]) + addend[i];
     for (i = 0; i < N; i++)
         VACC_M[i] = (short)(addend[i]);
+    for (i = 0; i < N; i++)
+        VACC_H[i] -= (product[i] < 0);
     for (i = 0; i < N; i++)
         VACC_H[i] += addend[i] >> 16;
     SIGNED_CLAMP_AM(VD);
