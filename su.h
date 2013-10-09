0282d9bac6e1325a42c41544f55dc668bf460426
unknown <rj150@hotmail.com>
2013-10-09 05:44:43 -0400

    uh, this was crap

--- su.h
+++ su.h
@@ -21,49 +21,6 @@
  */
 static int SR[32];
 
-/*
- * system control register indexing pointer table
- * This is particularly useful for directly executing MFC0.
- * MTC0 is too complicated to make regular use of this.
- * I cannot recreate as an array; they are engraved in zilmar plugin module.
- */
-#if 0
-static const unsigned long **CR[32] = {
-    &RSP.SP_MEM_ADDR_REG,
-    &RSP.SP_DRAM_ADDR_REG,
-    &RSP.SP_RD_LEN_REG,
-    &RSP.SP_WR_LEN_REG,
-    &RSP.SP_STATUS_REG,
-    &RSP.SP_DMA_FULL_REG,
-    &RSP.SP_DMA_BUSY_REG,
-    &RSP.SP_SEMAPHORE_REG, /* also "SP_RESERVED" */
-    &RSP.DPC_START_REG,
-    &RSP.DPC_END_REG,
-    &RSP.DPC_CURRENT_REG,
-    &RSP.DPC_STATUS_REG,
-    &RSP.DPC_CLOCK_REG,
-    &RSP.DPC_BUFBUSY_REG,
-    &RSP.DPC_PIPEBUSY_REG,
-    &RSP.DPC_TMEM_REG,
-    NULL, /* Usually there are 32 scalar CPU control registers. */
-    NULL, /* In fact, RSP assembler manual defines 32 indices, but lists 16. */
-    NULL, /* Conflicting sources and undefined doc reference leave this gray. */
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL,
-    NULL
-};
-#endif
-
 static void res_S(void)
 {
     message("RESERVED", 3);
