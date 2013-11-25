8607b1625d14ea9acba3f115977a88ee69ae0887
unknown <rj150@hotmail.com>
2013-11-25 18:18:00 -0500

    fixed jittery 3-D textures bug in World Driver Championship

--- vu/vsubc.h
+++ vu/vsubc.h
@@ -2,15 +2,18 @@
 
 INLINE static void set_bo(short* VD, short* VS, short* VT)
 { /* set CARRY and borrow out from difference */
+    long dif[N];
     register int i;
 
+    for (i = 0; i < N; i++)
+        dif[i] = (unsigned short)(VS[i]) - (unsigned short)(VT[i]);
     for (i = 0; i < N; i++)
         VACC_L[i] = VS[i] - VT[i];
     vector_copy(VD, VACC_L);
     for (i = 0; i < N; i++)
         ne[i] = (VS[i] != VT[i]);
     for (i = 0; i < N; i++)
-        co[i] = ((unsigned short)VS[i] < (unsigned short)VT[i]);
+        co[i] = (dif[i] < 0);
     return;
 }
 
