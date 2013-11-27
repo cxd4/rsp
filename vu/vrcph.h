87210c71a523482eb5d8aa5eb82468048514beac
unknown <rj150@hotmail.com>
2013-11-26 19:15:19 -0500

    cleanups and rewrites to the vector divide class

--- vu/vrcph.h
+++ vu/vrcph.h
@@ -6,6 +6,6 @@ static void VRCPH(int vd, int de, int vt, int e)
     DivIn = VR[vt][e & 07] << 16;
     SHUFFLE_VECTOR(VACC_L, VR[vt], e);
     VR[vd][de &= 07] = DivOut >> 16;
-    DPH = 1;
+    DPH = SP_DIV_PRECISION_DOUBLE;
     return;
 }
