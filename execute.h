cxd4/rsp

commit c089310109a8f945e7f641b41d447c3d9ee122bc

saturated opcode jump table to more direct management

execute.h
@@ -19,7 +19,10 @@ void run_task(void)
     PC = *RSP.SP_PC_REG & 0x00000FFC;
     while ((*RSP.SP_STATUS_REG & 0x00000001) == 0x00000000)
     {
-        inst.W = *(unsigned int *)(RSP.IMEM + PC);
+        const unsigned long inst = *(long *)(RSP.IMEM + PC);
+        const unsigned short imm = *(short *)(RSP.IMEM + PC + HES(0x002));
+        const short IW_upper_16  = *(short *)(RSP.IMEM + PC + HES(0x000));
+
 #ifdef EMULATE_STATIC_PC
         if (stage != 0) /* stage == 1 */
         {
@@ -33,16 +36,403 @@ void run_task(void)
         }
 #endif
 #ifdef SP_EXECUTE_LOG
-        step_SP_commands(inst.W);
+        step_SP_commands(inst);
 #endif
-        if (inst.W >> 25 == 0x25) /* is a VU instruction */
+        if (inst >> 25 == 0x25) /* is a VU instruction */
         {
-            COP2_C2[inst.R.func](inst.R.sa, inst.R.rd, inst.R.rt, inst.R.rs&15);
+            const int opcode = inst % 64; /* inst.R.func */
+            const int vd = (inst & 0x000007FF) >> 6; /* inst.R.sa */
+            const int vs = imm >> 11; /* inst.R.rd */
+            const int vt = IW_upper_16 & 31; /* inst.R.rt */
+            const int e  = (inst & 0x01FFFFFF) >> 21; /* rs & 0xF */
+
+            COP2_C2[opcode](vd, vs, vt, e);
         }
         else
         {
-         /* SR[0] = 0x00000000; // already handled on per-instruction basis */
-            EX_SCALAR[inst.W >> 26][inst.W>>sub_op_table[inst.W >> 26] & 037]();
+            const int op = inst >> 26;
+            const int rs = inst >> 21; /* &= 31 */
+            const int rt = IW_upper_16 & 31;
+            const int rd = imm >> 11;
+            const int sa = (inst/*& 0x000007FF*/) >> 6;
+            const int func = (inst & 0x0000003F) >> 0;
+            const int element = (sa & 31) >> 1;
+            const int base = rs & 31;
+
+#if (0)
+            SR[0] = 0x00000000; /* already handled on per-instruction basis */
+#endif
+            switch (op)
+            {
+                signed int offset;
+                register unsigned long addr;
+
+                case 000: /* SPECIAL */
+                    if (imm == 0)
+                        CONTINUE
+                    switch (func)
+                    {
+                        case 000: /* SLL */
+                            SR[rd] = SR[rt] << MASK_SA(imm >> 6);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 002: /* SRL */
+                            SR[rd] = (unsigned)(SR[rt]) >> MASK_SA(imm >> 6);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 003: /* SRA */
+                            SR[rd] = (signed)(SR[rt]) >> MASK_SA(imm >> 6);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 004: /* SLLV */
+                            SR[rd] = SR[rt] << MASK_SA(SR[rs]);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 006: /* SRLV */
+                            SR[rd] = (unsigned)(SR[rt]) >> MASK_SA(SR[rs]);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 007: /* SRAV */
+                            SR[rd] = (signed)(SR[rt]) >> MASK_SA(SR[rs]);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 011: /* JALR */
+                            SR[rd] = (*RSP.SP_PC_REG + LINK_OFF) & 0x00000FFC;
+                            SR[0] = 0x00000000;
+                        case 010: /* JR */
+                            set_PC(SR[rs]);
+                            CONTINUE
+                        case 015: /* BREAK */
+                            *RSP.SP_STATUS_REG |= 0x00000003; /* BROKE | HALT */
+                            if (*RSP.SP_STATUS_REG & 0x00000040)
+                            { /* SP_STATUS_INTR_BREAK */
+                                *RSP.MI_INTR_REG |= 0x00000001;
+                                RSP.CheckInterrupts();
+                            }
+                            return;
+                        case 040: /* ADD */
+                        case 041: /* ADDU */
+                            SR[rd] = SR[rs] + SR[rt];
+                            SR[0] = 0x00000000; /* needed for Rareware ucodes */
+                            CONTINUE
+                        case 042: /* SUB */
+                        case 043: /* SUBU */
+                            SR[rd] = SR[rs] - SR[rt];
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 044: /* AND */
+                            SR[rd] = SR[rs] & SR[rt];
+                            SR[0] = 0x00000000; /* needed for Rareware ucodes */
+                            CONTINUE
+                        case 045: /* OR */
+                            SR[rd] = SR[rs] | SR[rt];
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 046: /* XOR */
+                            SR[rd] = SR[rs] ^ SR[rt];
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 047: /* NOR */
+                            SR[rd] = ~(SR[rs] | SR[rt]);
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 052: /* SLT */
+                            SR[rd] = ((signed)(SR[rs]) < (signed)(SR[rt]));
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        case 053: /* SLTU */
+                            SR[rd] = ((unsigned)(SR[rs]) < (unsigned)(SR[rt]));
+                            SR[0] = 0x00000000;
+                            CONTINUE
+                        default:
+                            res_S();
+                            CONTINUE
+                    }
+                    CONTINUE
+                case 001: /* REGIMM */
+                    switch (rt)
+                    {
+                        case 020: /* BLTZAL */
+                            SR[31] = (*RSP.SP_PC_REG + LINK_OFF) & 0x00000FFC;
+                        case 000: /* BLTZ */
+                            if (!(SR[rs & 31] < 0))
+                                CONTINUE
+                            set_PC(*RSP.SP_PC_REG + 4*imm + SLOT_OFF);
+                            CONTINUE
+                        case 021: /* BGEZAL */
+                            SR[31] = (*RSP.SP_PC_REG + LINK_OFF) & 0x00000FFC;
+                        case 001: /* BGEZ */
+                            if (!(SR[rs & 31] >= 0))
+                                CONTINUE
+                            set_PC(*RSP.SP_PC_REG + 4*imm + SLOT_OFF);
+                            CONTINUE
+                        default:
+                            res_S();
+                            CONTINUE
+                    }
+                    CONTINUE
+                case 003: /* JAL */
+                    SR[31] = (*RSP.SP_PC_REG + LINK_OFF) & 0x00000FFC;
+                case 002: /* J */
+                    set_PC(4*imm);
+                    CONTINUE
+                case 004: /* BEQ */
+                    if (!(SR[rs & 31] == SR[rt]))
+                        CONTINUE
+                    set_PC(*RSP.SP_PC_REG + 4*imm + SLOT_OFF);
+                    CONTINUE
+                case 005: /* BNE */
+                    if (!(SR[rs & 31] != SR[rt]))
+                        CONTINUE
+                    set_PC(*RSP.SP_PC_REG + 4*imm + SLOT_OFF);
+                    CONTINUE
+                case 006: /* BLEZ */
+                    if (!((signed)SR[rs & 31] <= 0x00000000))
+                        CONTINUE
+                    set_PC(*RSP.SP_PC_REG + 4*imm + SLOT_OFF);
+                    CONTINUE
+                case 007: /* BGTZ */
+                    if (!((signed)SR[rs & 31] >  0x00000000))
+                        CONTINUE
+                    set_PC(*RSP.SP_PC_REG + 4*imm + SLOT_OFF);
+                    CONTINUE
+                case 010: /* ADDI */
+                case 011: /* ADDIU */
+                    SR[rt] = SR[rs & 31] + (signed short)(imm);
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 012: /* SLTI */
+                    SR[rt] = ((signed)(SR[rs & 31]) < (signed short)(imm));
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 013: /* SLTIU */
+                    SR[rt] = ((unsigned)(SR[rs & 31]) < imm);
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 014: /* ANDI */
+                    SR[rt] = SR[rs & 31] & imm;
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 015: /* ORI */
+                    SR[rt] = SR[rs & 31] | imm;
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 016: /* XORI */
+                    SR[rt] = SR[rs & 31] ^ imm;
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 017: /* LUI */
+                    SR[rt] = imm << 16;
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 020: /* COP0 */
+                    switch (rs & 31)
+                    {
+                        case 000: /* MFC0 */
+                            MFC0(rt, rd & 0xF);
+                            CONTINUE
+                        case 004: /* MTC0 */
+                            MTC0[rd & 0xF](rt);
+                            CONTINUE
+                        default:
+                            res_S();
+                            CONTINUE
+                    }
+                    CONTINUE
+                case 022: /* COP2 */
+                    switch (rs & 31)
+                    {
+                        case 000: /* MFC2 */
+                            MFC2(rt, rd, element);
+                            CONTINUE
+                        case 002: /* CFC2 */
+                            CFC2(rt, rd);
+                            CONTINUE
+                        case 004: /* MTC2 */
+                            MTC2(rt, rd, element);
+                            CONTINUE
+                        case 006: /* CTC2 */
+                            CTC2(rt, rd);
+                            CONTINUE
+                        default:
+                            res_S();
+                            CONTINUE
+                    }
+                    CONTINUE
+                case 040: /* LB */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    SR[rt] = RSP.DMEM[BES(addr)];
+                    SR[rt] = (signed char)(SR[rt]);
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 041: /* LH */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    if (addr%0x004 == 0x003)
+                    {
+                        SR_B(rt, 2) = RSP.DMEM[addr - BES(0x000)];
+                        addr = (addr + 0x00000001) & 0x00000FFF;
+                        SR_B(rt, 3) = RSP.DMEM[addr + BES(0x000)];
+                        SR[rt] = (signed short)(SR[rt]);
+                    }
+                    else
+                    {
+                        addr -= HES(0x000)*(addr%0x004 - 1);
+                        SR[rt] = *(signed short *)(RSP.DMEM + addr);
+                    }
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 043: /* LW */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    if (addr%0x004 != 0x000)
+                        ULW(rt, addr);
+                    else
+                        SR[rt] = *(long *)(RSP.DMEM + addr);
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 044: /* LBU */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    SR[rt] = RSP.DMEM[BES(addr)];
+                    SR[rt] = (unsigned char)(SR[rt]);
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 045: /* LHU */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    if (addr%0x004 == 0x003)
+                    {
+                        SR_B(rt, 2) = RSP.DMEM[addr - BES(0x000)];
+                        addr = (addr + 0x00000001) & 0x00000FFF;
+                        SR_B(rt, 3) = RSP.DMEM[addr + BES(0x000)];
+                        SR[rt] = (unsigned short)(SR[rt]);
+                    }
+                    else
+                    {
+                        addr -= HES(0x000)*(addr%0x004 - 1);
+                        SR[rt] = *(unsigned short *)(RSP.DMEM + addr);
+                    }
+                    SR[0] = 0x00000000;
+                    CONTINUE
+                case 050: /* SB */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    RSP.DMEM[BES(addr)] = (unsigned char)(SR[rt]);
+                    CONTINUE
+                case 051: /* SH */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    if (addr%0x004 == 0x003)
+                    {
+                        RSP.DMEM[addr - BES(0x000)] = SR_B(rt, 2);
+                        addr = (addr + 0x00000001) & 0x00000FFF;
+                        RSP.DMEM[addr + BES(0x000)] = SR_B(rt, 3);
+                        CONTINUE
+                    }
+                    addr -= HES(0x000)*(addr%0x004 - 1);
+                    *(short *)(RSP.DMEM + addr) = (short)(SR[rt]);
+                    CONTINUE
+                case 053: /* SW */
+                    offset = (signed short)(imm);
+                    addr = (SR[base] + offset) & 0x00000FFF;
+                    if (addr%0x004 != 0x000)
+                        USW(rt, addr);
+                    else
+                        *(long *)(RSP.DMEM + addr) = SR[rt];
+                    CONTINUE
+                case 062: /* LWC2 */
+                    offset = SE(imm, 6);
+                    switch (rd)
+                    {
+                        case 000: /* LBV */
+                            LBV(rt, element, offset, base);
+                            CONTINUE
+                        case 001: /* LSV */
+                            LSV(rt, element, offset, base);
+                            CONTINUE
+                        case 002: /* LLV */
+                            LLV(rt, element, offset, base);
+                            CONTINUE
+                        case 003: /* LDV */
+                            LDV(rt, element, offset, base);
+                            CONTINUE
+                        case 004: /* LQV */
+                            LQV(rt, element, offset, base);
+                            CONTINUE
+                        case 005: /* LRV */
+                            LRV(rt, element, offset, base);
+                            CONTINUE
+                        case 006: /* LPV */
+                            LPV(rt, element, offset, base);
+                            CONTINUE
+                        case 007: /* LUV */
+                            LUV(rt, element, offset, base);
+                            CONTINUE
+                        case 010: /* LHV */
+                            LHV(rt, element, offset, base);
+                            CONTINUE
+                        case 011: /* LFV */
+                            LFV(rt, element, offset, base);
+                            CONTINUE
+                        case 013: /* LTV */
+                            LTV(rt, element, offset, base);
+                            CONTINUE
+                        default:
+                            res_S();
+                            CONTINUE
+                    }
+                    CONTINUE
+                case 072: /* SWC2 */
+                    offset = SE(imm, 6);
+                    switch (rd)
+                    {
+                        case 000: /* SBV */
+                            SBV(rt, element, offset, base);
+                            CONTINUE
+                        case 001: /* SSV */
+                            SSV(rt, element, offset, base);
+                            CONTINUE
+                        case 002: /* SLV */
+                            SLV(rt, element, offset, base);
+                            CONTINUE
+                        case 003: /* SDV */
+                            SDV(rt, element, offset, base);
+                            CONTINUE
+                        case 004: /* SQV */
+                            SQV(rt, element, offset, base);
+                            CONTINUE
+                        case 005: /* SRV */
+                            SRV(rt, element, offset, base);
+                            CONTINUE
+                        case 006: /* SPV */
+                            SPV(rt, element, offset, base);
+                            CONTINUE
+                        case 007: /* SUV */
+                            SUV(rt, element, offset, base);
+                            CONTINUE
+                        case 010: /* SHV */
+                            SHV(rt, element, offset, base);
+                            CONTINUE
+                        case 011: /* SFV */
+                            SFV(rt, element, offset, base);
+                            CONTINUE
+                        case 012: /* SWV */
+                            SWV(rt, element, offset, base);
+                            CONTINUE
+                        case 013: /* STV */
+                            STV(rt, element, offset, base);
+                            CONTINUE
+                        default:
+                            res_S();
+                            CONTINUE
+                    }
+                    CONTINUE
+                default:
+                    res_S();
+                    CONTINUE
+            }
         }
 #ifndef EMULATE_STATIC_PC
         if (stage == 2) /* branch phase of scheduler */

rsp.c
@@ -15,9 +15,9 @@ EXPORT void CALL CloseDLL(void)
 static const char DLL_about[] =
     "RSP Interpreter by Iconoclast&&ECHO."\
     "&&ECHO "\
-    "Thanks for test RDP:  Jabo, ziggy, and angrylion\n"\
-    "RSP driver examples:  bpoint, zilmar, and Ville Linde\n"\
-    "Helpful shenanigans:  MarathonMan, dsx, and mudlord";
+    "Thanks for test RDP:  Jabo, ziggy, angrylion\n"\
+    "RSP driver examples:  bpoint, zilmar, Ville Linde\n"\
+    "Helpful shenanigans:  mudlord, MarathonMan, Garteal";
 EXPORT void CALL DllAbout(HWND hParent)
 {
     hParent = NULL;
@@ -29,14 +29,9 @@ EXPORT void CALL DllConfig(HWND hParent)
     hParent = NULL;
     system("sp_cfgui"); /* This launches an EXE by default (if not, BAT/CMD). */
     update_conf(CFG_FILE);
-    if (CFG_QUEUE_E_DRAM)
-        export_DRAM();
-    if (CFG_QUEUE_E_DMEM)
-        export_data_cache();
-    if (CFG_QUEUE_E_IMEM) /* to-do:  Make it also write the disassembly? */
-        export_instruction_cache();
-    if (CFG_TRACE_RSP_REGISTERS) /* to-do:  Make this counter of delay slots? */
-        trace_RSP_registers();
+    export_data_cache();
+    export_instruction_cache();
+    trace_RSP_registers();
     return;
 }
 EXPORT unsigned int CALL DoRspCycles(unsigned int cycles)
@@ -97,6 +92,8 @@ strcpy(
     PluginInfo -> MemoryBswaped = 1;
     return;
 }
+
+unsigned long* CR[16];
 EXPORT void CALL InitiateRSP(RSP_INFO Rsp_Info, unsigned int *CycleCount)
 {
     if (CycleCount != NULL) /* cycle-accuracy not doable with today's hosts */
@@ -112,6 +109,22 @@ EXPORT void CALL InitiateRSP(RSP_INFO Rsp_Info, unsigned int *CycleCount)
 
     RSP = Rsp_Info;
     *RSP.SP_PC_REG = 0x04001000 & 0x00000FFF; /* task init bug on Mupen64 */
+    CR[0x0] = RSP.SP_MEM_ADDR_REG;
+    CR[0x1] = RSP.SP_DRAM_ADDR_REG;
+    CR[0x2] = RSP.SP_RD_LEN_REG;
+    CR[0x3] = RSP.SP_WR_LEN_REG;
+    CR[0x4] = RSP.SP_STATUS_REG;
+    CR[0x5] = RSP.SP_DMA_FULL_REG;
+    CR[0x6] = RSP.SP_DMA_BUSY_REG;
+    CR[0x7] = RSP.SP_SEMAPHORE_REG;
+    CR[0x8] = RSP.DPC_START_REG;
+    CR[0x9] = RSP.DPC_END_REG;
+    CR[0xA] = RSP.DPC_CURRENT_REG;
+    CR[0xB] = RSP.DPC_STATUS_REG;
+    CR[0xC] = RSP.DPC_CLOCK_REG;
+    CR[0xD] = RSP.DPC_BUFBUSY_REG;
+    CR[0xE] = RSP.DPC_PIPEBUSY_REG;
+    CR[0xF] = RSP.DPC_TMEM_REG;
     return;
 }
 EXPORT void CALL InitiateRSPDebugger(DEBUG_INFO DebugInfo)

bench.h
@@ -1,7 +1,7 @@
 /******************************************************************************\
 * Project:  Simple Vector Unit Benchmark                                       *
 * Authors:  Iconoclast                                                         *
-* Release:  2013.11.26                                                         *
+* Release:  2013.12.01                                                         *
 * License:  none (public domain)                                               *
 \******************************************************************************/
 #ifndef _BENCH_H
@@ -88,8 +88,6 @@ const char* notice_finished =
     "Finished writing benchmark results.\n"\
     "Check working emulator directory for \"sp_bench.txt\".";
 
-unsigned char t_DMEM[0xFFF + 1], t_IMEM[0xFFF + 1];
-
 EXPORT void CALL DllTest(HWND hParent)
 {
     FILE* log;
@@ -102,8 +100,6 @@ EXPORT void CALL DllTest(HWND hParent)
         message("Cannot run RSP tests while playing!", 3);
         return;
     }
-    inst.W = 0x00000000;
-    inst.R.rs = 0x8; /* just to shut up VSAW illegal element warnings */
 
     message(notice_starting, 1);
     log = fopen("sp_bench.txt", "w");

config.h
@@ -12,6 +12,12 @@
 #define WAIT_FOR_CPU_HOST
 #define EMULATE_STATIC_PC
 
+#ifdef EMULATE_STATIC_PC
+#define CONTINUE    {continue;}
+#else
+#define CONTINUE    {break;}
+#endif
+
 #if (0)
 #define SP_EXECUTE_LOG
 #define VU_EMULATE_SCALAR_ACCUMULATOR_READ

rsp.h
@@ -109,13 +109,13 @@ static int MFC0_count[32];
 /* Keep one C0 MF status read count for each scalar register. */
 #endif
 
-#include "su.h"
-#include "vu/vu.h"
-
 extern void step_SP_commands(unsigned long inst);
 extern void export_SP_memory(void);
 extern void trace_RSP_registers(void);
 static FILE *output_log;
+
+#include "su.h"
+#include "vu/vu.h"
 
 /* Allocate the RSP CPU loop to its own functional space. */
 extern void run_task(void);
@@ -183,26 +183,10 @@ void export_instruction_cache(void)
     message("Finished IMEM export.  Look for \"rcpcache.ihex\".", 1);
     return;
 }
-void export_DRAM(void)
-{
-    FILE* out;
-    register unsigned long addr;
-    const int m = 0x7FFF % sizeof(unsigned int); /* swap mask */
-    const int MiB = 8; /* to-do:  any way to detect RDRAM size in MB? */
-    const int limit = MiB * 1024 * 1024;
-
-    out = fopen("RDRAM.BIN", "wb");
-    for (addr = 0x000000; addr < limit; addr += 0x000001)
-        fputc(RSP.RDRAM[(addr % limit) ^ m], out);
-    fclose(out);
-    message("Finished DRAM export.  Look for \"RDRAM.BIN\".", 1);
-    return;
-}
 void export_SP_memory(void)
-{ /* cache memory and dynamic RAM shared by CPU */
+{
     export_data_cache();
     export_instruction_cache();
-    export_DRAM();
     return;
 }
 
@@ -288,7 +272,7 @@ void trace_RSP_registers(void)
  * Just like we have the scalar 16 system control registers for the RSP CP0,
  * we have also a tiny group of special-purpose, vector control registers.
  */
-    fprintf(out, "\n$vco:  0x%04X\n", get_VCO());
+    fprintf(out, "$vco:  0x%04X\n", get_VCO());
     fprintf(out, "$vcc:  0x%04X\n", get_VCC());
     fprintf(out, "$vce:  0x%02X\n\n", get_VCE());
 
@@ -310,7 +294,6 @@ void trace_RSP_registers(void)
     /* MovIn:  This reg. might exist for VMOV, but it is obsolete to emulate. */
     fprintf(out, DPH ? "DPH:  true\n" : "DPH:  false\n");
     fclose(out);
-    message("Finished tracing RSP registers.  Look for \"SP_STATE.TXT\".", 1);
     return;
 }
 #endif

commit 111ea1b187ca8ec0371ef59c2f788933e26ca9e5

Fix instruction decoding for systems with sizeof(long) > 4
This regression was introduced in c089310109a8f945e7f641b41d447c3d9ee122bc
("saturated opcode jump table to more direct management")

execute.h
@@ -21,9 +21,9 @@ NOINLINE void run_task(void)
     PC = FIT_IMEM(*RSP.SP_PC_REG);
     while ((*RSP.SP_STATUS_REG & 0x00000001) == 0x00000000)
     {
-        register unsigned long inst;
+        register uint32_t inst;
 
-        inst = *(long *)(RSP.IMEM + FIT_IMEM(PC));
+        inst = *(uint32_t *)(RSP.IMEM + FIT_IMEM(PC));
 #ifdef EMULATE_STATIC_PC
         PC = (PC + 0x004);
 EX:
@@ -440,7 +440,7 @@ EX:
 #else
         continue;
 BRANCH:
-        inst = *(long *)(RSP.IMEM + FIT_IMEM(PC));
+        inst = *(uint32_t *)(RSP.IMEM + FIT_IMEM(PC));
         PC = temp_PC & 0x00000FFC;
         goto EX;
 #endif

rsp.c
@@ -2,6 +2,7 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <stdint.h>
 #include <string.h>
 #include "Rsp_#1.1.h"
 #include "rsp.h"