cxd4/rsp

commit 87210c71a523482eb5d8aa5eb82468048514beac

cleanups and rewrites to the vector divide class

vu/divrom.h
@@ -1049,4 +1049,57 @@ static const unsigned short div_ROM[1024] = {
     0x0040,
     0x6A64
 };
+
+enum {
+    SP_DIV_SQRT_NO,
+    SP_DIV_SQRT_YES
+};
+enum {
+    SP_DIV_PRECISION_SINGLE = 0,
+    SP_DIV_PRECISION_DOUBLE = 1,
+    SP_DIV_PRECISION_CURRENT
+};
+
+INLINE static void do_div(int data, int sqrt, int precision)
+{
+    long addr;
+    int fetch;
+    int shift;
+
+    if (precision == SP_DIV_PRECISION_SINGLE)
+        data = (data < 0) ? -data : +data;
+    if (precision == SP_DIV_PRECISION_DOUBLE && data < 0)
+        data = (data >= -32768) ? -data : ~data;
+
+/*
+ * Note, from the code just above, that data cannot be negative.
+ * (data >= 0) is unconditionally forced by the above algorithm.
+ */
+    addr = data;
+    if (data == 0x00000000)
+    {
+        shift = (precision == SP_DIV_PRECISION_SINGLE) ? 16 : 0;
+        addr = addr << shift;
+    }
+    else
+        for (shift = 0; addr >= 0x00000000; addr <<= 1, shift++);
+    addr = (addr >> 22) & 0x000001FF;
+
+    if (sqrt == SP_DIV_SQRT_YES)
+    {
+        addr &= 0x000001FE;
+        addr |= 0x00000200 | (shift & 1);
+    }
+    fetch = div_ROM[addr];
+    shift ^= 31; /* flipping shift direction from left- to right- */
+    shift >>= (sqrt == SP_DIV_SQRT_YES);
+    DivOut = (0x40000000 | (fetch << 14)) >> shift;
+    if (DivIn == 0) /* corner case:  overflow via division by zero */
+        DivOut = 0x7FFFFFFF;
+    else if (DivIn == -32768) /* corner case:  signed underflow barrier */
+        DivOut = 0xFFFF0000;
+    else
+        DivOut ^= (DivIn < 0) ? ~0 : 0;
+    return;
+}
 #endif

vu/vrcp.h
@@ -1,42 +1,12 @@
 #include "vu.h"
 #include "divrom.h"
 
-INLINE static void do_rcp(int data)
-{
-    unsigned int addr;
-    int fetch;
-    int shift = 32;
-
-    DPH = 0;
-    if (data < 0)
-        data = -data;
-    do
-    {
-        --shift;
-        if (data & (1 << shift))
-            goto FOUND_MSB;
-    } while (shift); /* while (shift > 0) or ((shift ^ 31) < 32) */
-    shift = 16 ^ 31; /* No bits found in (data == 0x00000000), so shift = 16. */
-FOUND_MSB:
-    shift ^= 31; /* Right-to-left shift direction conversion. */
-    addr = (data << shift) >> 22;
-    fetch = div_ROM[addr &= 0x000001FF];
-    shift ^= 31; /* Flipped shift direction back to right-. */
-    DivOut = (0x40000000 | (fetch << 14)) >> shift;
-    if (DivIn < 0)
-        DivOut = ~DivOut;
-    else if (DivIn == 0) /* corner case:  overflow via division by zero */
-        DivOut = 0x7FFFFFFF;
-    else if (DivIn == -32768) /* corner case:  signed underflow barrier */
-        DivOut = 0xFFFF0000;
-    return;
-}
-
 static void VRCP(int vd, int de, int vt, int e)
 {
     DivIn = (int)VR[vt][e & 07];
-    do_rcp(DivIn);
+    do_div(DivIn, SP_DIV_SQRT_NO, SP_DIV_PRECISION_SINGLE);
     SHUFFLE_VECTOR(VACC_L, VR[vt], e);
     VR[vd][de &= 07] = (short)DivOut;
+    DPH = SP_DIV_PRECISION_SINGLE;
     return;
 }

vu/vrcph.h
@@ -6,6 +6,6 @@ static void VRCPH(int vd, int de, int vt, int e)
     DivIn = VR[vt][e & 07] << 16;
     SHUFFLE_VECTOR(VACC_L, VR[vt], e);
     VR[vd][de &= 07] = DivOut >> 16;
-    DPH = 1;
+    DPH = SP_DIV_PRECISION_DOUBLE;
     return;
 }

vu/vrcpl.h
@@ -1,43 +1,13 @@
 #include "vu.h"
 #include "divrom.h"
 
-INLINE static void do_rcpl(int data)
-{
-    unsigned int addr;
-    int fetch;
-    int shift = 32;
-
-    if (data < 0)
-        data = -data - (data < -32768); /* -(x) if >=; ~(x) if < */
-    do
-    {
-        --shift;
-        if (data & (1 << shift))
-            goto FOUND_MSB;
-    } while (shift); /* while (shift > 0) or ((shift ^ 31) < 32) */
-    shift = 31 - 16*DPH; /* if (data == 0) shift = DPH ? 16 ^ 31 : 0 ^ 31; */
-FOUND_MSB:
-    DPH = 0;
-    shift ^= 31; /* Right-to-left shift direction conversion. */
-    addr = (data << shift) >> 22;
-    fetch = div_ROM[addr &= 0x000001FF];
-    shift ^= 31; /* Flipped shift direction back to right-. */
-    DivOut = (0x40000000 | (fetch << 14)) >> shift;
-    if (DivIn < 0)
-        DivOut = ~DivOut;
-    else if (DivIn == 0) /* corner case:  overflow via division by zero */
-        DivOut = 0x7FFFFFFF;
-    else if (DivIn == -32768) /* corner case:  signed underflow barrier */
-        DivOut = 0xFFFF0000;
-    return;
-}
-
 static void VRCPL(int vd, int de, int vt, int e)
 {
     DivIn &= -DPH;
     DivIn |= (unsigned short)VR[vt][e & 07];
-    do_rcpl(DivIn);
+    do_div(DivIn, SP_DIV_SQRT_NO, DPH);
     SHUFFLE_VECTOR(VACC_L, VR[vt], e);
     VR[vd][de &= 07] = (short)DivOut;
+    DPH = SP_DIV_PRECISION_SINGLE;
     return;
 }

vu/vrsqh.h
@@ -6,6 +6,6 @@ static void VRSQH(int vd, int de, int vt, int e)
     DivIn = VR[vt][e & 07] << 16;
     SHUFFLE_VECTOR(VACC_L, VR[vt], e);
     VR[vd][de &= 07] = DivOut >> 16;
-    DPH = 1;
+    DPH = SP_DIV_PRECISION_DOUBLE;
     return;
 }

vu/vrsql.h
@@ -1,46 +1,13 @@
 #include "vu.h"
 #include "divrom.h"
 
-INLINE static void do_rsql(int data)
-{
-    unsigned int addr;
-    int fetch;
-    int shift = 32;
-
-    if (data < 0)
-        data = -data - (data < -32768); /* -(x) if >=; ~(x) if < */
-    do
-    {
-        --shift;
-        if (data & (1 << shift))
-            goto FOUND_MSB;
-    } while (shift); /* while (shift > 0) or ((shift ^ 31) < 32) */
-    shift = 31 - 16*DPH; /* if (data == 0) shift = DPH ? 16 ^ 31 : 0 ^ 31; */
-FOUND_MSB:
-    DPH = 0;
-    shift ^= 31; /* Right-to-left shift direction conversion. */
-    addr = (data << shift) >> 22;
-    addr &= 0x000001FE;
-    addr |= 0x00000200 | (shift & 1);
-    fetch = div_ROM[addr];
-    shift ^= 31; /* Flipped shift direction back to right-. */
-    shift >>= 1;
-    DivOut = (0x40000000 | (fetch << 14)) >> shift;
-    if (DivIn < 0)
-        DivOut = ~DivOut;
-    else if (DivIn == 0) /* corner case:  overflow via division by zero */
-        DivOut = 0x7FFFFFFF;
-    else if (DivIn == -32768) /* corner case:  signed underflow barrier */
-        DivOut = 0xFFFF0000;
-    return;
-}
-
 static void VRSQL(int vd, int de, int vt, int e)
 {
     DivIn &= -DPH;
     DivIn |= (unsigned short)VR[vt][e & 07];
-    do_rsql(DivIn);
+    do_div(DivIn, SP_DIV_SQRT_YES, DPH);
     SHUFFLE_VECTOR(VACC_L, VR[vt], e);
     VR[vd][de &= 07] = (short)DivOut;
+    DPH = SP_DIV_PRECISION_SINGLE;
     return;
 }

commit 0faed67a64335217ebbf6ed19f5579070d8e167d

Fix vector divides on systems with sizeof(long) > 4
The wrong div operations result in wrong transformations in many roms.

This regression was introduced in 87210c71a523482eb5d8aa5eb82468048514beac
("cleanups and rewrites to the vector divide class").

vu/divrom.h
@@ -1062,7 +1062,7 @@ enum {
 
 INLINE static void do_div(int data, int sqrt, int precision)
 {
-    long addr;
+    int32_t addr;
     int fetch;
     int shift;
 