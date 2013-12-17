4392ebde428136f85ad08375fe6af8588e7b7c40
Sven Eckelmann <sven@narfation.org>
2013-12-17 13:36:56 +0100

    Add an explicit public domain dedication statement
    The process of dedicating a piece of work under the public domain is not the
    same under different legal systems. It is possible that different rights are
    given away depending on the origin. Sometimes even the dedication of the work
    under public domain is not possible at all. CC0 tries to provide an explicit
    way to waive all rights to still provide a secure way for other parties to use
    this work [1] and provides a fallback when parts of the license may be judged
    invalid under any jurisdiction.
    
    [1] http://creativecommons.org/about/cc0

--- su.h
+++ su.h
@@ -2,7 +2,15 @@
 * Project:  MSP Emulation Table for Scalar Unit Operations                     *
 * Authors:  Iconoclast                                                         *
 * Release:  2013.12.10                                                         *
-* License:  none (public domain)                                               *
+* License:  CC0 Public Domain Dedication                                       *
+*                                                                              *
+* To the extent possible under law, the author(s) have dedicated all copyright *
+* and related and neighboring rights to this software to the public domain     *
+* worldwide. This software is distributed without any warranty.                *
+*                                                                              *
+* You should have received a copy of the CC0 Public Domain Dedication along    *
+* with this software.                                                          *
+* If not, see <http://creativecommons.org/publicdomain/zero/1.0/>.             *
 \******************************************************************************/
 #ifndef _SU_H
 #define _SU_H
