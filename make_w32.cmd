25a238fd5deade36a013baa0abf4203854257ece
Iconoclast <cxd4@uniplex.cat-vs-internet.gov>
2017-08-14 10:08:23 -0400

    Force absolute invocation paths.

--- make_w32.cmd
+++ make_w32.cmd
@@ -40,28 +40,28 @@ mkdir vu
 cd %MinGW%\bin
 
 ECHO Compiling C source code...
-cc -S %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
-cc -S %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
-cc -S %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
-cc -S %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
-cc -S %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
-cc -S %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
-cc -S %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
-cc -S %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
+%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
 ECHO.
 
 ECHO Assembling compiled sources...
-as --statistics -o %obj%\module.o %obj%\module.asm
-as --statistics -o %obj%\su.o     %obj%\su.asm
-as --statistics -o %obj%\vu\vu.o  %obj%\vu\vu.asm
-as -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
-as -o %obj%\vu\add.o      %obj%\vu\add.asm
-as -o %obj%\vu\select.o   %obj%\vu\select.asm
-as -o %obj%\vu\logical.o  %obj%\vu\logical.asm
-as -o %obj%\vu\divide.o   %obj%\vu\divide.asm
+%MinGW%\bin\as.exe --statistics -o %obj%\module.o %obj%\module.asm
+%MinGW%\bin\as.exe --statistics -o %obj%\su.o     %obj%\su.asm
+%MinGW%\bin\as.exe --statistics -o %obj%\vu\vu.o  %obj%\vu\vu.asm
+%MinGW%\bin\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
+%MinGW%\bin\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
+%MinGW%\bin\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
+%MinGW%\bin\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
+%MinGW%\bin\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
 ECHO.
 
 ECHO Linking assembled object files...
-ld --shared -e _DllMain@12 -o %obj%\rspdebug.dll %OBJ_LIST% %MinGW%\lib\libkernel32.a
-strip -o %obj%/rsp.dll %obj%/rspdebug.dll
+%MinGW%\bin\ld.exe --shared -e _DllMain@12 -o %obj%\rspdebug.dll %OBJ_LIST% %MinGW%\lib\libkernel32.a
+%MinGW%\bin\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
 PAUSE
