b29a6ae7dd105516700c6b708d994aeee721ea30
Iconoclast <cxd4@cat-vs-internet.gov>
2018-03-16 00:04:51 -0400

    cleaned up Windows command shell build scripts

--- make_w32.cmd
+++ make_w32.cmd
@@ -1,7 +1,18 @@
 @ECHO OFF
 TITLE MinGW Compiler Suite Invocation
 
+REM If you have MinGW on a different drive letter or installed at a custom path
+REM (or just not yet installed at all), this build script may not work out of
+REM the box for most Windows users.  Alternatives include MinGW-w32 or trying
+REM to execute the Unix shell script "make.sh" from Windows 10+ or Git Bash.
+
+REM The following line is the only one you should ever need to change.
 set MinGW=C:\MinGW
+
+set lib=%MinGW%\lib
+set bin=%MinGW%\bin
+set inc=%MinGW%\include
+
 REM set rsp=%USERPROFILE%\rsp
 set rsp=%CD%
 set obj=%rsp%\obj
@@ -37,31 +48,31 @@ mkdir obj
 cd obj
 mkdir vu
 )
-cd %MinGW%\bin
+cd %bin%
 
 ECHO Compiling C source code...
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
-%MinGW%\bin\gcc.exe -S %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
+%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
 ECHO.
 
 ECHO Assembling compiled sources...
-%MinGW%\bin\as.exe --statistics -o %obj%\module.o %obj%\module.asm
-%MinGW%\bin\as.exe --statistics -o %obj%\su.o     %obj%\su.asm
-%MinGW%\bin\as.exe --statistics -o %obj%\vu\vu.o  %obj%\vu\vu.asm
-%MinGW%\bin\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
-%MinGW%\bin\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
-%MinGW%\bin\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
-%MinGW%\bin\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
-%MinGW%\bin\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
+%bin%\as.exe --statistics -o %obj%\module.o %obj%\module.asm
+%bin%\as.exe --statistics -o %obj%\su.o     %obj%\su.asm
+%bin%\as.exe --statistics -o %obj%\vu\vu.o  %obj%\vu\vu.asm
+%bin%\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
+%bin%\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
+%bin%\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
+%bin%\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
+%bin%\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
 ECHO.
 
 ECHO Linking assembled object files...
-%MinGW%\bin\ld.exe --shared -e _DllMain@12 -o %obj%\rspdebug.dll -L %MinGW%\lib %OBJ_LIST% -lkernel32
-%MinGW%\bin\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
+%bin%\ld.exe --shared -e _DllMain@12 -o %obj%\rspdebug.dll -L %lib% %OBJ_LIST% -lkernel32
+%bin%\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
 PAUSE
--- make_w64.cmd
+++ make_w64.cmd
@@ -1,8 +1,20 @@
 @ECHO OFF
 TITLE MinGW Compiler Suite Invocation
 
-set MinGW=C:\msys64\mingw64
-set lib=%MinGW%\x86_64-w64-mingw32\lib
+REM If you have installed MinGW-w64 without using MSYS2 to obtain the package
+REM (or just not yet installed at all), this build script may not work out of
+REM the box for most Windows users.  If you have Cygwin instead or whatever
+REM else, be sure to adjust the path below, or execute "make.sh" in a Git shell.
+
+REM The following line is the only one you should ever need to change.
+set mingw64=C:\msys64\mingw64
+
+set mingw32=%mingw64%\..\mingw32
+REM set lib=%mingw32%\i686-w64-mingw32
+
+set lib=%mingw64%\x86_64-w64-mingw32\lib
+set bin=%mingw64%\bin
+set inc=%lib%\..\include
 
 REM set rsp=%USERPROFILE%\rsp
 set rsp=%CD%
@@ -37,31 +49,31 @@ mkdir obj
 cd obj
 mkdir vu
 )
-cd %MinGW%\bin
+cd %bin%
 
 ECHO Compiling C source code...
-%MinGW%\bin\gcc.exe -S -Os %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
-%MinGW%\bin\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+%bin%\gcc.exe -S -Os %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
+%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
 ECHO.
 
 ECHO Assembling compiled sources...
-%MinGW%\bin\as.exe -o %obj%\module.o      %obj%\module.asm
-%MinGW%\bin\as.exe -o %obj%\su.o          %obj%\su.asm
-%MinGW%\bin\as.exe -o %obj%\vu\vu.o       %obj%\vu\vu.asm
-%MinGW%\bin\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
-%MinGW%\bin\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
-%MinGW%\bin\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
-%MinGW%\bin\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
-%MinGW%\bin\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
+%bin%\as.exe -o %obj%\module.o      %obj%\module.asm
+%bin%\as.exe -o %obj%\su.o          %obj%\su.asm
+%bin%\as.exe -o %obj%\vu\vu.o       %obj%\vu\vu.asm
+%bin%\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
+%bin%\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
+%bin%\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
+%bin%\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
+%bin%\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
 ECHO.
 
 ECHO Linking assembled object files...
-%MinGW%\bin\ld.exe --shared -e DllMain -o %obj%\rspdebug.dll -L%lib% %OBJ_LIST% -lkernel32
-%MinGW%\bin\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
+%bin%\ld.exe --shared -e DllMain -o %obj%\rspdebug.dll -L%lib% %OBJ_LIST% -lkernel32
+%bin%\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
 PAUSE
