2b0585acfc74302a6ad6bacdc787c0ba83ca9af3
Iconoclast <cxd4@cat-vs-internet.gov>
2018-03-16 22:07:40 -0400

    Turn ECHO on for translation unit compiles only.

--- make_w32.cmd
+++ make_w32.cmd
@@ -51,28 +51,30 @@ mkdir vu
 cd /D %bin%
 
 ECHO Compiling C source code...
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
-%bin%\gcc.exe -S %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+@ECHO ON
+gcc -S %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
+gcc -S %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
+gcc -S %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
+gcc -S %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
+gcc -S %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
+gcc -S %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
+gcc -S %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
+gcc -S %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+@ECHO OFF
 ECHO.
 
 ECHO Assembling compiled sources...
-%bin%\as.exe --statistics -o %obj%\module.o %obj%\module.asm
-%bin%\as.exe --statistics -o %obj%\su.o     %obj%\su.asm
-%bin%\as.exe --statistics -o %obj%\vu\vu.o  %obj%\vu\vu.asm
-%bin%\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
-%bin%\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
-%bin%\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
-%bin%\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
-%bin%\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
+as -o %obj%\module.o            %obj%\module.asm
+as -o %obj%\su.o                %obj%\su.asm
+as -o %obj%\vu\vu.o             %obj%\vu\vu.asm
+as -o %obj%\vu\multiply.o       %obj%\vu\multiply.asm
+as -o %obj%\vu\add.o            %obj%\vu\add.asm
+as -o %obj%\vu\select.o         %obj%\vu\select.asm
+as -o %obj%\vu\logical.o        %obj%\vu\logical.asm
+as -o %obj%\vu\divide.o         %obj%\vu\divide.asm
 ECHO.
 
 ECHO Linking assembled object files...
-%bin%\ld.exe --shared -e _DllMain@12 -o %obj%\rspdebug.dll -L %lib% %OBJ_LIST% -lmsvcrt
-%bin%\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
+ld --shared -e _DllMain@12 -o %obj%\rspdebug.dll -L %lib% %OBJ_LIST% -lmsvcrt
+strip -o %obj%\rsp.dll %obj%\rspdebug.dll --strip-all
 PAUSE
--- make_w64.cmd
+++ make_w64.cmd
@@ -53,28 +53,30 @@ mkdir vu
 cd /D %bin%
 
 ECHO Compiling C source code...
-%bin%\gcc.exe -S -Os %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
-%bin%\gcc.exe -S -O3 %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+@ECHO ON
+gcc -S -Os %C_FLAGS% -o %obj%\module.asm      %rsp%\module.c
+gcc -S -O3 %C_FLAGS% -o %obj%\su.asm          %rsp%\su.c
+gcc -S -O3 %C_FLAGS% -o %obj%\vu\vu.asm       %rsp%\vu\vu.c
+gcc -S -O3 %C_FLAGS% -o %obj%\vu\multiply.asm %rsp%\vu\multiply.c
+gcc -S -O3 %C_FLAGS% -o %obj%\vu\add.asm      %rsp%\vu\add.c
+gcc -S -O3 %C_FLAGS% -o %obj%\vu\select.asm   %rsp%\vu\select.c
+gcc -S -O3 %C_FLAGS% -o %obj%\vu\logical.asm  %rsp%\vu\logical.c
+gcc -S -O3 %C_FLAGS% -o %obj%\vu\divide.asm   %rsp%\vu\divide.c
+@ECHO OFF
 ECHO.
 
 ECHO Assembling compiled sources...
-%bin%\as.exe -o %obj%\module.o      %obj%\module.asm
-%bin%\as.exe -o %obj%\su.o          %obj%\su.asm
-%bin%\as.exe -o %obj%\vu\vu.o       %obj%\vu\vu.asm
-%bin%\as.exe -o %obj%\vu\multiply.o %obj%\vu\multiply.asm
-%bin%\as.exe -o %obj%\vu\add.o      %obj%\vu\add.asm
-%bin%\as.exe -o %obj%\vu\select.o   %obj%\vu\select.asm
-%bin%\as.exe -o %obj%\vu\logical.o  %obj%\vu\logical.asm
-%bin%\as.exe -o %obj%\vu\divide.o   %obj%\vu\divide.asm
+as -o %obj%\module.o            %obj%\module.asm
+as -o %obj%\su.o                %obj%\su.asm
+as -o %obj%\vu\vu.o             %obj%\vu\vu.asm
+as -o %obj%\vu\multiply.o       %obj%\vu\multiply.asm
+as -o %obj%\vu\add.o            %obj%\vu\add.asm
+as -o %obj%\vu\select.o         %obj%\vu\select.asm
+as -o %obj%\vu\logical.o        %obj%\vu\logical.asm
+as -o %obj%\vu\divide.o         %obj%\vu\divide.asm
 ECHO.
 
 ECHO Linking assembled object files...
-%bin%\ld.exe --shared -e DllMain -o %obj%\rspdebug.dll -L%lib64% %OBJ_LIST% -lmsvcrt
-%bin%\strip.exe -o %obj%/rsp.dll %obj%/rspdebug.dll
+ld --shared -e DllMain -o %obj%\rspdebug.dll -L%lib64% %OBJ_LIST% -lmsvcrt
+strip -o %obj%\rsp.dll %obj%\rspdebug.dll --strip-all
 PAUSE
